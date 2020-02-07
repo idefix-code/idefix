632d422db1136c2b1cd9a136a367f8cca4f83fa1
Geoffroy Lesur <geoffroy.lesur@obs.ujf-grenoble.fr>
2020-02-07 10:29:52 +0100

    another debug option

--- src/data.cpp
+++ src/data.cpp
@@ -5,7 +5,7 @@ Data::Data() {
     // Do nothing
 }
 Data::Data(Grid &grid) {
-    std::cout << "Building data Array\n";
+    std::cout << "Building data Array with size " << NVAR << "x" << grid.np_tot[KDIR] << "x" << grid.np_tot[JDIR] << "x" << grid.np_tot[IDIR] << std::endl;
 
     Vc = IdefixArray4D<real>("Vc", NVAR, grid.np_tot[KDIR], grid.np_tot[JDIR], grid.np_tot[IDIR]);
     Uc = IdefixArray4D<real>("Uc", NVAR, grid.np_tot[KDIR], grid.np_tot[JDIR], grid.np_tot[IDIR]);

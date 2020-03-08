8dd10e9f2a53796e05fb0eca1698377397987bcb
Geoffroy Lesur <geoffroy@MacPro-des-Lesur.local>
2020-03-08 21:56:28 +0100

    added parameter input from Athena

--- src/idefix.hpp
+++ src/idefix.hpp
@@ -9,8 +9,8 @@ using Layout = Kokkos::LayoutRight;
 
 // Define the kind of loop we want (see loop.hpp for details)
 //#define  INDEX_LOOP
-#define  MDRANGE_LOOP
-//#define  SIMD_LOOP
+//#define  MDRANGE_LOOP
+#define  SIMD_LOOP
 //#define  TP_INNERX_LOOP
 //#define  TPTTRTVR_LOOP
 

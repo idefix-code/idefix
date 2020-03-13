cca66050c052fe7e0a7988a6b1701aac6d506e55
Geoffroy Lesur <geoffroy.lesur@obs.ujf-grenoble.fr>
2020-03-13 15:40:42 +0100

    added sod shock tube test

---
+++ Test/HD/sod/definitions.hpp
@@ -0,0 +1,7 @@
+#define     COMPONENTS      1
+#define     DIMENSIONS      1
+#define     HAVE_ENERGY     1
+
+#define     GEOMETRY        CARTESIAN
+// Order of the scheme. 1=donnor cell, 2= linear reconstruction
+#define     ORDER           2

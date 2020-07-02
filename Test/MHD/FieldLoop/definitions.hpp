e698829490b118c412c5f4e47bf374e54e1e09c3
Geoffroy Lesur <geoffroy.lesur@obs.ujf-grenoble.fr>
2020-07-02 14:22:50 +0200

    new test: Field line advection

---
+++ Test/MHD/FieldLoop/definitions.hpp
@@ -0,0 +1,8 @@
+#define     COMPONENTS      3
+#define     DIMENSIONS      3
+#define     HAVE_ENERGY     1
+#define	    MHD		    YES
+
+#define     GEOMETRY        CARTESIAN
+// Order of the scheme. 1=donnor cell, 2= linear reconstruction
+#define     ORDER           2

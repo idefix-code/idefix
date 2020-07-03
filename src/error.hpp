0b7c34ffb7bce120f97db6d6693c1c3e6d275fb8
Geoffroy Lesur <geoffroy.lesur@obs.ujf-grenoble.fr>
2020-07-03 10:28:33 +0200

    error.hpp apparently absent...

--- src/error.hpp
+++ src/error.hpp
@@ -3,7 +3,7 @@
 
 #define		IDEFIX_ERROR(ERROR_MESSAGE)		ErrorHandler(ERROR_CRITICAL, ERROR_MESSAGE, std::string(__func__) , __LINE__, std::string(__FILE__))
 #define     IDEFIX_WARNING(WARNING_MESSAGE) ErrorHandler(ERROR_WARNING, WARNING_MESSAGE, std::string(__func__) , __LINE__, std::string(__FILE__))
-
+ 
 void ErrorHandler (const int,
 			  std::stringstream &, 
 			  std::string , 

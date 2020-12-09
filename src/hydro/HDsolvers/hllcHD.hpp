c951dcf2ba3a0d3cd653b3b9892f382c1655f086
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2020-12-09 08:58:04 +0100

    lacking sod tests with other Riemann solvers fixed gamma for hllc

--- src/hydro/HDsolvers/hllcHD.hpp
+++ src/hydro/HDsolvers/hllcHD.hpp
@@ -36,7 +36,8 @@ void Hydro::HllcHD() {
   IdefixArray4D<real> PrimR = this->PrimR;
   IdefixArray4D<real> Flux = this->FluxRiemann;
   IdefixArray3D<real> cMax = this->cMax;
-
+  
+  real gamma = this->gamma;
   real gamma_m1 = this->gamma - ONE_F;
   real C2Iso = this->C2Iso;
 

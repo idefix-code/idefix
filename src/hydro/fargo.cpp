a8740a0c9227bf7420daeeacec2dcceb9ebfdeda
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2021-03-25 16:26:53 +0100

    initial push for 2D MHD

--- src/hydro/fargo.cpp
+++ src/hydro/fargo.cpp
@@ -236,5 +236,177 @@ void Fargo::ShiftSolution(const real t, const real dt) {
   // move back scratch space into Uc
   Kokkos::deep_copy(Uc,scrh);
 
+#if MHD == YES
+  IdefixArray3D<real> Vs = hydro->Vs;
+  IdefixArray3D<real> ex = hydro->emf.Ex1;
+  IdefixArray3D<real> ey = hydro->emf.Ex2;
+  IdefixArray3D<real> ez = hydro->emf.Ex3;
+  IdefixArray1D<real> x1m = data->xl[IDIR];
+  IdefixArray1D<real> x2m = data->xl[JDIR];
+
+
+  int ioffset{0};
+  int joffset{0};
+  int koffset{0};
+
+
+  #if GEOMETRY == CARTESIAN || GEOMETRY == POLAR
+    IdefixArray3D<real> ek = ez;
+    ioffset = IOFFSET;
+  #elif GEOMETRY == SPHERICAL
+    IdefixArray3D<real> ek = ey;
+    koffset = KOFFSET;
+  #endif
+
+  idefix_for("Fargo:ComputeEk",
+    data->beg[KDIR],data->end[KDIR]+koffset,
+    data->beg[JDIR],data->end[JDIR]+joffset,
+    data->beg[IDIR],data->end[IDIR]+ioffset,
+    KOKKOS_LAMBDA(int k, int j, int i) {
+      real w,dphi;
+      int s;
+      #if GEOMETRY == CARTESIAN
+        w = 0.5*(meanV(k,i-1)+meanV(k,i));
+        dphi = dx2(j);
+        s = j;
+      #elif GEOMETRY == POLAR
+        w = 0.5*(meanV(k,i-1)+meanV(k,i))/x1m(i);
+        dphi = dx2(j);
+        s = j;
+      #elif GEOMETRY == SPHERICAL
+        w = 0.5*(meanV(j,i-1)+meanV(j,i))/(x1m(i)*SIN(x2(j)));
+        dphi = dx3(k);
+        s = k;
+      #endif
+
+      // Compute the offset in phi, modulo the full domain size
+      real dL = std::fmod(w*dt, Lphi);
+
+      // Translate this into # of cells
+      int m = static_cast<int> (std::floor(dL/dphi+HALF_F));
+
+      // get the remainding shift
+      real eps = dL/dphi - m;
+
+      // origin index before the shift
+      // Note the trick to get a positive module i%%n = (i%n + n)%n;
+      int n = send-sbeg;
+
+      // so is the "origin" index
+      int so = sbeg + ((s-m-sbeg)%n+n)%n;
+
+      // compute shifted indices, taking into account the fact that we're periodic
+      int sop1 = sbeg + (so+1-sbeg)%(send-sbeg);
+      int som1 = sbeg + (so-1-sbeg)%(send-sbeg);
+      int som2 = sbeg + (so-2-sbeg)%(send-sbeg);
+
+      // Compute EMF due to the shift via second order reconstruction
+      real dqm, dqp, dq;
+
+      #if GEOMETRY == CARTESIAN || GEOMETRY == POLAR
+        if(eps>=ZERO_F) {
+          // Compute extrapolated ek
+          dqm = Vs(BX1s,k,som1,i) - Vs(BX1s,k,som2,i);
+          dqp = Vs(BX1s,k,so,i) - Vs(BX1s,k,som1,i);
+          dq = (dqp*dqm > ZERO_F ? TWO_F*dqp*dqm/(dqp + dqm) : ZERO_F);
+          ek(k,s,i) = Vs(BX1s,k,som1,i) + 0.5*dq*(1.0-eps);
+        } else {
+          dqm = Vs(BX1s,k,so,i) - Vs(BX1s,k,som1,i);
+          dqp = Vs(BX1s,k,sop1,i) - Vs(BX1s,k,so,i);
+          dq = (dqp*dqm > ZERO_F ? TWO_F*dqp*dqm/(dqp + dqm) : ZERO_F);
+          ek(k,s,i) = Vs(BX1s,k,so,i) - 0.5*dq*(1.0+eps);
+        }
+        if(m>0) {
+          for(int ss = s-m ; ss < s ; ss++) {
+            int sc = sbeg + ((ss-sbeg)%n+n)%n;
+            ek(k,s,i) += Vs(BX1s,k,sc,i);
+          }
+        } else {
+          for(int ss = s ; ss < s-m ; ss++) {
+            int sc = sbeg + ((ss-sbeg)%n+n)%n;
+            ek(k,s,i) -= Vs(BX1s,k,sc,i);
+          }
+        }
+
+
+      #elif GEOMETRY == SPHERICAL
+      if(eps>=ZERO_F) {
+        // Compute extrapolated ek
+        dqm = Vs(BX1s,som1,j,i) - Vs(BX1s,som2,j,i);
+        dqp = Vs(BX1s,so,j,i) - Vs(BX1s,som1,j,i);
+        dq = (dqp*dqm > ZERO_F ? TWO_F*dqp*dqm/(dqp + dqm) : ZERO_F);
+        ek(s,j,i) = Vs(BX1s,som1,j,i) + 0.5*dq*(1.0-eps);
+      } else {
+        dqm = Vs(BX1s,so,j,i) - Vs(BX1s,som1,j,i);
+        dqp = Vs(BX1s,sop1,j,i) - Vs(BX1s,so,j,i);
+        dq = (dqp*dqm > ZERO_F ? TWO_F*dqp*dqm/(dqp + dqm) : ZERO_F);
+        ek(s,j,i) = Vs(BX1s,so,j,i) - 0.5*dq*(1.0+eps);
+      }
+      if(m>0) {
+        for(int ss = s-m ; ss < s ; ss++) {
+          int sc = sbeg + ((ss-sbeg)%n+n)%n;
+          ek(s,j,i) += Vs(BX1s,sc,j,i);
+        }
+      } else {
+        for(int ss = s ; ss < s-m ; ss++) {
+          int sc = sbeg + ((ss-sbeg)%n+n)%n;
+          ek(s,j,i) -= Vs(BX1s,sc,j,i);
+        }
+      }
+
+      #endif  // GEOMETRY
+
+      ek(k,j,i) *= dphi;
+    });
+
+  // Update field components according to the computed EMFS
+
+  idefix_for("Fargo::EvolvMagField",
+             data->beg[KDIR],data->end[KDIR]+KOFFSET,
+             data->beg[JDIR],data->end[JDIR]+JOFFSET,
+             data->beg[IDIR],data->end[IDIR]+IOFFSET,
+    KOKKOS_LAMBDA (int k, int j, int i) {
+      real rhsx1, rhsx2, rhsx3;
+
+#if GEOMETRY == CARTESIAN || GEOMETRY == POLAR
+      Vs(BX1s,k,j,i) += -  (ez(k,j+1,i) - ez(k,j,i) ) / dx2(j);
+
+#elif GEOMETRY == SPHERICAL
+      Vs(BX1s,k,j,i) += -  (ey(k+1,j,i) - ey(k,j,i) ) / dx3(k);
+#endif
+
+#if GEOMETRY == CARTESIAN
+      Vs(BX2s,k,j,i) += D_EXPAND(    0.0                          ,
+                            + (ez(k,j,i+1) - ez(k,j,i)) / dx1(i)  ,
+                            - (ex(k+1,j,i) - ex(k,j,i)) / dx3(k)  );
+#elif GEOMETRY == POLAR
+      Vs(BX2s,k,j,i) += D_EXPAND(    0.0                                          ,
+                            + (x1m(i+1) * ez(k,j,i+1) - x1m(i)*ez(k,j,i)) / dx1(i)  ,
+                            - x1(i) *  (ex(k+1,j,i) - ex(k,j,i)) / dx3(k)  );
+#elif GEOMETRY == SPHERICAL
+    #if DIMENSIONS == 3
+      Vs(BX2s,k,j,i) += (ex(k+1,j,i) - ex(k,j,i)) / dx3(k);
+    #endif
+#endif // GEOMETRY
+
+#if DIMENSIONS == 3
+  #if GEOMETRY == CARTESIAN || GEOMETRY == POLAR
+    Vs(BX3s,k,j,i) +=  (ex(k,j+1,i) - ex(k,j,i) ) / dx2(j);
+  #elif GEOMETRY == SPHERICAL
+    real A1p = x1p(i)*x1p(i);
+    real A1m = x1m(i)*x1m(i);
+    real A2m = FABS(sinx2m(j));
+    real A2p = FABS(sinx2m(j+1));
+    Vs(BX3s,k,j,i) += sinx2(j) * (A1p * ez(k,j,i+1) - A1m * ez(k,j,i))/(x1(i)*dx1(i))
+                      - (A2p * ex(k,j+1,i) - A2m * ex(k,j,i))/dx2(j);
+  #endif
+#endif// DIMENSIONS
+  });
+
+  // Rebuild the cell-centered field components
+  this->hydro->ReconstructVcField(Uc);
+
+#endif // MHD
+
   idfx::popRegion();
 }

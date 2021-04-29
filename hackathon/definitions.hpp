0fa037d99e90f0e52a54d5e2548e0cdedc0a714c
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2021-04-29 13:59:12 +0200

    commiting a base setup for the hackathon

---
+++ hackathon/definitions.hpp
@@ -0,0 +1,8 @@
+#define     COMPONENTS      3
+#define     DIMENSIONS      3
+
+#define     GEOMETRY        POLAR
+// Order of the scheme. 1=donnor cell, 2= linear reconstruction
+#define     ORDER           2
+// #define DEBUG
+#define     SMALL_PRESSURE_TEMPERATURE   (0.05)

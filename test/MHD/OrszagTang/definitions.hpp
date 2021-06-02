f018b8e3a35e8e716706b35d4756b69616b751d1
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2021-06-02 11:32:29 +0200

    on the fly argument determination for Riemann solvers

--- test/MHD/OrszagTang/definitions.hpp
+++ test/MHD/OrszagTang/definitions.hpp
@@ -1,7 +1,6 @@
 #define     COMPONENTS      2
 #define     DIMENSIONS      2
 
-
 #define     GEOMETRY        CARTESIAN
 // Order of the scheme. 1=donnor cell, 2= linear reconstruction
 #define     ORDER           2

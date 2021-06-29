05627ec26e7378adfe6ffbcc3683a66e42754891
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2021-06-29 09:02:12 +0200

    missing files for SB tests

---
+++ test/HD/ShearingBox/definitions.hpp
@@ -0,0 +1,8 @@
+#define     COMPONENTS      3
+#define     DIMENSIONS      3
+
+//#define    EMF_AVERAGE   ARITHMETIC
+
+#define     GEOMETRY        CARTESIAN
+// Order of the scheme. 1=donnor cell, 2= linear reconstruction
+#define     ORDER           2

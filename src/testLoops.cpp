c559550cfee1fe4370f1264674130984259e666c
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2021-09-23 14:40:37 +0200

    fix auto tune for cuda

--- src/testLoops.cpp
+++ src/testLoops.cpp
@@ -37,13 +37,49 @@ void testLoopType(DataBlock &data, Setup &setup, Input &input, int numLoops, Loo
   idfx::cout << "Perfs are " << 1/tintegration << " cell updates/second" << std::endl;
 
 }
+
+// Convert lp integer into text
+std::string LoopText(int lp) {
+  std::string sout;
+
+  switch(static_cast<LoopPattern>(lp)) {
+    case LoopPattern::SIMDFOR:
+      sout = std::string("SIMD");
+      break;
+    case LoopPattern::RANGE:
+      sout = std::string("RANGE");
+      break;
+    case LoopPattern::MDRANGE:
+      sout = std::string("MDRANGE");
+      break;
+    case LoopPattern::TPX:
+      sout = std::string("TPX");
+      break;
+    case LoopPattern::TPTTRTVR:
+      sout = std::string("TPTTRTVR");
+      break;
+  }
+  return(sout);
+}
 void testLoops(DataBlock &data, Setup &setup, Input &input, int numLoops) {
   LoopPattern oldLoop = idfx::defaultLoopPattern;
+  // Do one dummy loop so that all is initialized
+  idfx::cout << "Initializing auto-tune" << std::endl;
+  testLoopType(data,setup,input,numLoops,oldLoop);
   for(int i = 0 ; i != static_cast<int>(LoopPattern::UNDEFINED) ; i++) {
     idfx::cout << "*************************************************" << std::endl;
-    idfx::cout << "Loop pattern " << i << std::endl;
-    testLoopType(data,setup,input,numLoops,static_cast<LoopPattern>(i));
+    idfx::cout << "Loop pattern " << LoopText(i) << "(" << i << ")" << std::endl;
+    #ifdef KOKKOS_ENABLE_CUDA
+    if (static_cast<LoopPattern>(i) != LoopPattern::SIMDFOR) {
+    #endif
+      testLoopType(data,setup,input,numLoops,static_cast<LoopPattern>(i));
+    #ifdef KOKKOS_ENABLE_CUDA
+    } else {
+      idfx::cout << "Not implemented in Cuda, skipping" << std::endl;
+    }
+    #endif
     idfx::cout << "*************************************************" << std::endl;
   }
   idfx::defaultLoopPattern = oldLoop;
 }
+

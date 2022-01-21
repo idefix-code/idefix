b9dd05021e28418f9e559a49949c02a01043b792
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2022-01-21 14:54:33 +0100

    init files for CSV reader

---
+++ src/utils/readCSV.cpp
@@ -0,0 +1,17 @@
+// ***********************************************************************************
+// Idefix MHD astrophysical code
+// Copyright(C) 2020-2022 Geoffroy R. J. Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
+// and other code contributors
+// Licensed under CeCILL 2.1 License, see COPYING for more information
+// ***********************************************************************************
+
+#ifndef UTILS_READCSV_HPP_
+#define UTILS_READCSV_HPP_
+
+#include "idefix.hpp"
+#include "readCSV.hpp"
+
+class ReadCSV {
+}
+
+#endif //UTILS_READCSV_HPP_

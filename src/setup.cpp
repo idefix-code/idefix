19bdd2c8ee90e2e45bc76da495c1d7dccdb9388b
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2022-10-03 17:26:34 +0200

    attempt at allowing the user to provide an optional Setup destructor

--- src/setup.cpp
+++ src/setup.cpp
@@ -8,11 +8,16 @@
 #include "setup.hpp"
 #include "idefix.hpp"
 
+// Default setup functions. These are automatically redefined if the user provide her/his
+// own implementation of the constructor, initflow and destructor
 
-Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output) {
+__attribute__((weak)) Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output) {
   IDEFIX_WARNING("Caution, this is the default Setup constructor and it does nothing!");
 }
 
-void Setup::InitFlow(DataBlock &data) {
+__attribute__((weak)) void Setup::InitFlow(DataBlock &data) {
   IDEFIX_ERROR("Please create your own setup.cpp following the documentation");
+}
+
+__attribute__((weak)) Setup::~Setup() {
 }

cd9590b62ac5d4db49308dbe7541c7a8d1f4b9bc
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2022-11-14 11:46:58 +0100

    re-enable shock flattening

--- src/hydro/slopeLimiter.hpp
+++ src/hydro/slopeLimiter.hpp
@@ -183,23 +183,22 @@ class SlopeLimiter {
         real dvp = Vc(nv,k,j,i)-Vc(nv,k-koffset,j-joffset,i-ioffset);
 
         real dv;
-        /*
         if(shockFlattening) {
           if(flags(k,j,i) == FlagShock::Shock) {
+            // Force slope limiter to minmod
             dv = MinModLim(dvp,dvm);
           } else {
             dv = PLMLim(dvp,dvm);
           }
         } else { // No shock flattening
-        */
           dv = PLMLim(dvp,dvm);
-        //}
+        }
 
         vL[nv] = Vc(nv,k-koffset,j-joffset,i-ioffset) + HALF_F*dv;
 
         dvm = dvp;
         dvp = Vc(nv,k+koffset,j+joffset,i+ioffset) - Vc(nv,k,j,i);
-/*
+
         if(shockFlattening) {
           if(flags(k,j,i) == FlagShock::Shock) {
             dv = MinModLim(dvp,dvm);
@@ -207,9 +206,8 @@ class SlopeLimiter {
             dv = PLMLim(dvp,dvm);
           }
         } else { // No shock flattening
-        */
           dv = PLMLim(dvp,dvm);
-        //}
+        }
 
         vR[nv] = Vc(nv,k,j,i) - HALF_F*dv;
       } else if constexpr(order == 3) {

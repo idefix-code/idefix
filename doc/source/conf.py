aa5820ac4f18fcb2fb60665e22b8a423a425f2ae
Clément Robert <cr52@protonmail.com>
2022-11-20 20:02:20 +0100

    MNT: auto clean unused Python imports with ruff

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
-import sphinx.ext.mathjax
 import textwrap
 
 # -- Project information -----------------------------------------------------

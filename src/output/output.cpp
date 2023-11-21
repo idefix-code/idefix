7a06db338f8fed58623fba3c9a6900ba07e42fc2
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2023-11-21 17:36:05 +0100

    remove unwanted filesystem dependence

--- src/output/output.cpp
+++ src/output/output.cpp
@@ -5,7 +5,6 @@
 // Licensed under CeCILL 2.1 License, see COPYING for more information
 // ***********************************************************************************
 
-#include <filesystem>
 #include "output.hpp"
 #include "dataBlock.hpp"
 #include "fluid.hpp"

4e98c4b9a0b731360a01e3b4789ca880f299b6d0
Clément Robert <cr52@protonmail.com>
2023-11-24 14:45:30 +0100

    RFC: simplify RUNTIME_CHECK_KERNEL's signature using Kokkos::printf

--- src/macros.hpp
+++ src/macros.hpp
@@ -173,25 +173,17 @@
     } \
   }
 
-  #if defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_CUDA)
-    // string formatting functions can't be accessed from GPU kernels,
-    // so we fallback to a simple error message
-    #define RUNTIME_CHECK_KERNEL(condition, fallback_msg, ...) { \
-      if(!(condition)) Kokkos::abort(fallback_msg); \
-    }
-  #else
-    #define RUNTIME_CHECK_KERNEL(condition, fallback_msg, ...) { \
-      if(!(condition)) { \
-        char msg[255]; \
-        snprintf(msg, sizeof(msg), __VA_ARGS__); \
-        Kokkos::abort(msg); \
-      } \
-    }
-  #endif // if defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_CUDA)
+  #define RUNTIME_CHECK_KERNEL(condition, ...) { \
+    if(!(condition)) { \
+      char msg[255]; \
+      Kokkos::printf(msg, __VA_ARGS__); \
+      Kokkos::abort(msg); \
+    } \
+  }
 
 #else
-  #define RUNTIME_CHECK_HOST(condition, msg, ...) {}
-  #define RUNTIME_CHECK_KERNEL(condition, fallback_msg, ...) {}
+  #define RUNTIME_CHECK_HOST(condition, ...) {}
+  #define RUNTIME_CHECK_KERNEL(condition, ...) {}
 #endif // ifdef RUNTIME_CHECKS
 
 

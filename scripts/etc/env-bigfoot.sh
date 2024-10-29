1d9ea2fb52ad2c9aca33ad917a956d4c572bb38f
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2024-10-29 16:22:59 +0100

    Adapt the Nix shell to avoid pointing at /bin and adding it to the PATH

--- scripts/etc/env-bigfoot.sh
+++ scripts/etc/env-bigfoot.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_HIP=ON -DKokkos_ARCH_VEGA90A=ON -DCMAKE_CXX_COMPILER=hipcc -DIdefix_MPI=ON )
+source /applis/site/nix.sh
+
+# IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_HIP=ON -DKokkos_ARCH_VEGA90A=ON -DCMAKE_CXX_COMPILER=hipcc )
+mkdir -p "$HOME/.nix-shell"
+ln -fs "$SHELL" "$HOME/.nix-shell/sh"
 
 function in_env() {
     local -a cmd=( "$@" )
-    NIX_BUILD_SHELL=$(which bash) nix-shell ~/idefix.drv --run "$(declare -p cmd); "'"${cmd[@]}"'
+    NIX_BUILD_SHELL="$HOME/.nix-shell/sh" nix-shell ~/idefix.drv --run "$(declare -p cmd); "'"${cmd[@]}"'
 }

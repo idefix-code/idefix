95f1f93c01446f2e034e123ac042a69f6ea6d738
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2024-10-29 16:38:41 +0100

    Avoid loading the build environment unless necessary

--- scripts/etc/env-bigfoot.sh
+++ scripts/etc/env-bigfoot.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-set +ue
-source /applis/site/nix.sh >/dev/null 2>&1
-set -ue
-
-# IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_HIP=ON -DKokkos_ARCH_VEGA90A=ON -DCMAKE_CXX_COMPILER=hipcc )
-mkdir -p "$HOME/.nix-shell"
-ln -fs "$(which bash)" "$HOME/.nix-shell/bash"
+function setup_env() {
+    set +ue
+    source /applis/site/nix.sh >/dev/null 2>&1
+    set -ue
+    
+    mkdir -p "$HOME/.nix-shell"
+    ln -fs "$(which bash)" "$HOME/.nix-shell/bash"
+}
 
 function in_env() {
     local -a cmd=( "$@" )

f632fe32597bb798192e3001502126f23e47e6a9
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2024-10-29 16:25:33 +0100

    Name it 'bash', to avoid weird POSIX shell behaviours

--- scripts/etc/env-bigfoot.sh
+++ scripts/etc/env-bigfoot.sh
@@ -3,9 +3,9 @@ source /applis/site/nix.sh
 
 # IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_HIP=ON -DKokkos_ARCH_VEGA90A=ON -DCMAKE_CXX_COMPILER=hipcc )
 mkdir -p "$HOME/.nix-shell"
-ln -fs "$SHELL" "$HOME/.nix-shell/sh"
+ln -fs "$(which bash)" "$HOME/.nix-shell/bash"
 
 function in_env() {
     local -a cmd=( "$@" )
-    NIX_BUILD_SHELL="$HOME/.nix-shell/sh" nix-shell ~/idefix.drv --run "$(declare -p cmd); "'"${cmd[@]}"'
+    NIX_BUILD_SHELL="$HOME/.nix-shell/bash" nix-shell ~/idefix.drv --run "$(declare -p cmd); "'"${cmd[@]}"'
 }

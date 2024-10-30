59be150f85844fa50198cca26c40752db2adc149
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2024-10-30 11:16:01 +0100

    Start handling compiling for specific GPUs

--- scripts/etc/env-bigfoot.sh
+++ scripts/etc/env-bigfoot.sh
@@ -8,6 +8,22 @@ function setup_env() {
     ln -fs "$(which bash)" "$HOME/.nix-shell/bash"
 }
 
+function set_gpu_options() {
+    local model="$1"; shift
+    case "$model" in
+        V100)
+            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON );;
+        A100)
+            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_AMPERE80=ON );;
+        H100)
+            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_HOPPER90=ON );;
+        '') ;;
+        *)
+            printf "Error: unknown gpu architecture '%s'\n" "$model"
+            return 1
+    esac
+}
+
 function in_env() {
     local -a cmd=( "$@" )
     local drvfile="$IDEFIX_DIR/scripts/etc/env-bigfoot.drv"

482de9dd5f1d9ceebb272b0516de47bb647d5163
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2024-10-30 14:32:35 +0100

    Try to use Nix to find Cuda

--- scripts/etc/env-bigfoot.nix
+++ scripts/etc/env-bigfoot.nix
@@ -1,4 +1,6 @@
 let nixpkgs = import <nixpkgs> {};
+    cudart = lib.getDev cudaPackages.cuda_cudart;
+
     inputs = with nixpkgs; [
       nur.repos.gricad.openmpi4
       nur.repos.gricad.ucx
@@ -6,6 +8,8 @@ let nixpkgs = import <nixpkgs> {};
       rocmPackages.clr
       rocmPackages.rocthrust
       rocmPackages.rocprim
+      cudaPackages.cuda_nvcc
+      cudart
       cmake
       gcc
       pkg-config
@@ -14,4 +18,5 @@ in nixpkgs.mkShell {
   buildInputs = inputs;
   LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath inputs;
   NIX_SHELL_PROMPT_TAG = "idefix";
+  IDEFIX_CUDA_INCLUDE = "${cudart}/include";
 }
--- scripts/etc/env-bigfoot.sh
+++ scripts/etc/env-bigfoot.sh
@@ -12,11 +12,11 @@ function set_gpu_options() {
     local model="$1"; shift
     case "$model" in
         V100)
-            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON );;
+            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON "-DCUDAToolkit_INCLUDE_DIR=$(in_env_raw 'echo $IDEFIX_CUDA_INCLUDE')" );;
         A100)
-            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_AMPERE80=ON );;
+            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_AMPERE80=ON "-DCUDAToolkit_INCLUDE_DIR=$(in_env_raw 'echo $IDEFIX_CUDA_INCLUDE')" );;
         H100)
-            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_HOPPER90=ON );;
+            IDEFIX_CMAKE_OPTIONS+=( -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_HOPPER90=ON "-DCUDAToolkit_INCLUDE_DIR=$(in_env_raw 'echo $IDEFIX_CUDA_INCLUDE')" );;
         '') ;;
         *)
             printf "Error: unknown gpu architecture '%s'\n" "$model"
@@ -24,13 +24,17 @@ function set_gpu_options() {
     esac
 }
 
-function in_env() {
-    local -a cmd=( "$@" )
+function in_env_raw() {
+    local -a cmd="$1"
     local drvfile="$IDEFIX_DIR/scripts/etc/env-bigfoot.drv"
     if [ ! -e "$drvfile" ]; then
         printf "Cacheing an Idefix shell derivation in %s\n" "$drvfile"
         nix-instantiate --add-root "$drvfile" "$IDEFIX_DIR/scripts/etc/env-bigfoot.nix"
     fi
-    printf "Running command: %s\n" "$*"
-    NIX_BUILD_SHELL="$HOME/.nix-shell/bash" nix-shell "$drvfile" --run "$(declare -p cmd); "'"${cmd[@]}"'
+    printf "Running command: %s\n" "$cmd" >&2
+    NIX_BUILD_SHELL="$HOME/.nix-shell/bash" nix-shell "$drvfile" --run "$cmd"
+}
+function in_env() {
+    local -a cmd=( "$@" )
+    in_env_raw "$(declare -p cmd); "'"${cmd[@]}"'
 }

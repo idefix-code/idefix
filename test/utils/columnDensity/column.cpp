e2f7a9a16bf97e2f76245757bbf91cdba188a120
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2025-01-12 18:07:36 +0100

    add a column Density example

---
+++ test/utils/columnDensity/column.cpp
@@ -0,0 +1,215 @@
+#include "column.hpp"
+#include "loop.hpp"
+
+Column::Column(int dir, int sign, int variable, DataBlock *data) : direction(dir), sign(sign), variable(variable) {
+  idfx::pushRegion("Column::Column");
+  this->np_tot = data->np_tot;
+  this->np_int = data->np_int;
+  this->beg = data->beg;
+
+  if(dir>= DIMENSIONS || dir < IDIR) IDEFIX_ERROR("Unknown direction for Column constructor");
+
+  // Allocate the array on which we do the average
+  this->ColumnArray = IdefixArray3D<real>("ColumnArray",np_tot[KDIR], np_tot[JDIR], np_tot[IDIR]);
+
+  // Elementary volumes and area
+  this->Volume = data->dV;
+  this->Area = data->A[dir];
+
+  // allocate helper  array
+  if(dir == IDIR) {
+    localSum = IdefixArray2D<real>("localSum",np_tot[KDIR], np_tot[JDIR]);
+  }
+  if(dir == JDIR) {
+    localSum = IdefixArray2D<real>("localSum",np_tot[KDIR], np_tot[IDIR]);
+  }
+  if(dir == KDIR) {
+    localSum = IdefixArray2D<real>("localSum",np_tot[JDIR], np_tot[IDIR]);
+  }
+  #ifdef WITH_MPI
+  // Create sub-MPI communicator dedicated to scan
+    int remainDims[3] = {false, false, false};
+    remainDims[dir] = true;
+    MPI_Cart_sub(data->mygrid->CartComm, remainDims, &this->ColumnComm);
+    MPI_Comm_rank(this->ColumnComm, &this->MPIrank);
+    MPI_Comm_size(this->ColumnComm, &this->MPIsize);
+
+    // create MPI class for boundary Xchanges
+    int ntarget = 0;
+    std::vector<int> mapVars;
+    mapVars.push_back(ntarget);
+
+    this->mpi.Init(data->mygrid, mapVars, data->nghost.data(), data->np_int.data());
+    this->nproc = data->mygrid->nproc;
+  #endif
+  idfx::popRegion();
+}
+
+void Column::ComputeColumn(IdefixArray4D<real> in) {
+  idfx::pushRegion("Column::ComputeColumn");
+  const int nk = np_int[KDIR];
+  const int nj = np_int[JDIR];
+  const int ni = np_int[IDIR];
+  const int kb = beg[KDIR];
+  const int jb = beg[JDIR];
+  const int ib = beg[IDIR];
+  const int ke = kb+nk;
+  const int je = jb+nj;
+  const int ie = ib+ni;
+
+  const int direction = this->direction;
+  auto column = this->ColumnArray;
+  auto dV = this->Volume;
+  auto A = this->Area;
+  auto localSum = this->localSum;
+
+  const int var = this->variable;
+  if(direction==IDIR) {
+    // Inspired from loop.hpp
+    Kokkos::parallel_for("ColumnX1", team_policy (nk*nj, Kokkos::AUTO),
+      KOKKOS_LAMBDA (member_type team_member) {
+        int k = team_member.league_rank() / nj;
+        int j = team_member.league_rank() - k*nj + jb;
+        k += kb;
+        Kokkos::parallel_scan(Kokkos::TeamThreadRange<>(team_member,ib,ie),
+          [=] (int i, real &partial_sum, bool is_final) {
+            partial_sum += in(var,k,j,i)*dV(k,j,i) / (0.5*(A(k,j,i)+A(k,j,i+1)));
+            if(is_final) column(k,j,i) = partial_sum;
+            //partial_sum += in(var,k,j,i)*dV(k,j,i) / (0.5*(A(k,j,i)+A(k,j,i+1)));
+          });
+      });
+    }
+    if(direction==JDIR) {
+      // Inspired from loop.hpp
+      Kokkos::parallel_for("ColumnX2", team_policy (nk*ni, Kokkos::AUTO),
+        KOKKOS_LAMBDA (member_type team_member) {
+          int k = team_member.league_rank() / ni;
+          int i = team_member.league_rank() - k*ni + ib;
+          k += kb;
+          Kokkos::parallel_scan(Kokkos::TeamThreadRange<>(team_member,jb,je),
+            [=] (int j, real &partial_sum, bool is_final) {
+              partial_sum += in(var,k,j,i)*dV(k,j,i) / (0.5*(A(k,j,i)+A(k,j+1,i)));
+              if(is_final) column(k,j,i) = partial_sum;
+          });
+      });
+    }
+    if(direction==KDIR) {
+      // Inspired from loop.hpp
+      Kokkos::parallel_for("ColumnX2", team_policy (nj*ni, Kokkos::AUTO),
+        KOKKOS_LAMBDA (member_type team_member) {
+          int j = team_member.league_rank() / ni;
+          int i = team_member.league_rank() - j*ni + ib;
+          j += jb;
+          Kokkos::parallel_scan(Kokkos::TeamThreadRange<>(team_member,kb,ke),
+            [=] (int k, real &partial_sum, bool is_final) {
+              partial_sum += in(var,k,j,i)*dV(k,j,i) / (0.5*(A(k,j,i)+A(k+1,j,i)));
+              if(is_final) column(k,j,i) = partial_sum;
+          });
+      });
+    }
+
+    #ifdef WITH_MPI
+    // Load the current sum
+      int dst,src;
+      MPI_Cart_shift(this->ColumnComm,0, 1, &src, &dst);
+      int size = localSum.extent(0)*localSum.extent(1);
+      if(MPIrank>0) {
+        MPI_Status status;
+        // Get the cumulative sum from previous processes
+        Kokkos::fence();
+        MPI_Recv(localSum.data(), size, realMPI, src, 20, ColumnComm, &status);
+        // Add this to our cumulative sum
+        idefix_for("Addsum",kb,ke,jb,je,ib,ie,
+          KOKKOS_LAMBDA(int k, int j, int i) {
+            if(direction == IDIR) column(k,j,i) += localSum(k,j);
+            if(direction == JDIR) column(k,j,i) += localSum(k,i);
+            if(direction == KDIR) column(k,j,i) += localSum(j,i);
+        });
+      } // rank =0
+      // Get the final sum
+      if(MPIrank < MPIsize-1) {
+        if(direction==IDIR) {
+          idefix_for("Loadsum",kb,ke,jb,je,
+            KOKKOS_LAMBDA(int k, int j) {
+              localSum(k,j) = column(k,j,ie-1);
+          });
+        }
+        if(direction==JDIR) {
+          idefix_for("Loadsum",kb,ke,ib,ie,
+            KOKKOS_LAMBDA(int k, int i) {
+              localSum(k,i) = column(k,je-1,i);
+          });
+        }
+        if(direction==KDIR) {
+          idefix_for("Loadsum",jb,je,ib,ie,
+            KOKKOS_LAMBDA(int j, int i) {
+              localSum(j,i) = column(ke-1,j,i);
+          });
+        }
+        // And send it
+        Kokkos::fence();
+        MPI_Send(localSum.data(),size, realMPI, dst, 20, ColumnComm);
+      } // MPIrank small enough
+    #endif
+    // If we need it backwards
+    if(sign<0) {
+      // Compute total cumulative sum in the last subdomain
+      if(direction == IDIR) {
+        idefix_for("Loadsum",kb,ke,jb,je,
+          KOKKOS_LAMBDA(int k, int j) {
+            localSum(k,j) = column(k,j,ie-1) + in(var,k,j,ie-1) * dV(k,j,ie-1) / (0.5*(A(k,j,ie-1)+A(k,j,ie)));
+        });
+      }
+      if(direction == JDIR) {
+        idefix_for("Loadsum",kb,ke,ib,ie,
+          KOKKOS_LAMBDA(int k, int i) {
+            localSum(k,i) = column(k,je-1,i) + in(var,k,je-1,i) * dV(k,je-1,i) / (0.5*(A(k,je-1,i)+A(k,je,i)));
+        });
+      }
+      if(direction == KDIR) {
+        idefix_for("Loadsum",jb,je,ib,ie,
+          KOKKOS_LAMBDA(int j, int i) {
+            localSum(j,i) = column(ke-1,j,i) + in(var,ke-1,j,i) * dV(ke-1,j,i) / (0.5*(A(ke-1,j,i)+A(ke,j,i)));
+        });
+      }
+
+      #ifdef WITH_MPI
+      Kokkos::fence();
+      MPI_Bcast(localSum.data(),size, realMPI, MPIsize-1, ColumnComm);
+      #endif
+      // All substract the local column from the full column
+
+      idefix_for("Subsum",kb,ke,jb,je,ib,ie,
+        KOKKOS_LAMBDA(int k, int j, int i) {
+          if(direction==IDIR) column(k,j,i) = localSum(k,j)-column(k,j,i);
+          if(direction==JDIR) column(k,j,i) = localSum(k,i)-column(k,j,i);
+          if(direction==KDIR) column(k,j,i) = localSum(j,i)-column(k,j,i);
+      });
+    } // sign <0
+    // Xchange boundary elements when using MPI to ensure that column
+    // density in the ghost zones are coherent
+    #ifdef WITH_MPI
+      // Create a 4D array that contains our column data
+      this->arr4D = IdefixArray4D<real> (column.data(), 1, this->np_tot[KDIR],
+                                                        this->np_tot[JDIR],
+                                                        this->np_tot[IDIR]);
+
+      for(int dir = 0 ; dir < DIMENSIONS ; dir++) {
+        // MPI Exchange data when needed
+        if(nproc[dir]>1) {
+          switch(dir) {
+            case 0:
+              this->mpi.ExchangeX1(this->arr4D);
+              break;
+            case 1:
+              this->mpi.ExchangeX2(this->arr4D);
+              break;
+            case 2:
+              this->mpi.ExchangeX3(this->arr4D);
+              break;
+          }
+        }
+      }
+    #endif
+  idfx::popRegion();
+}
---
+++ test/utils/columnDensity/setup.cpp
@@ -0,0 +1,154 @@
+#include "idefix.hpp"
+#include "setup.hpp"
+#include "column.hpp"
+
+
+Column *columnX1Left;
+Column *columnX1Right;
+Column *columnX2Left;
+Column *columnX2Right;
+Column *columnX3Left;
+Column *columnX3Right;
+
+// Analyse data to check that column density works as expected
+void Analysis(DataBlock & data) {
+
+  DataBlockHost d(data);
+
+  columnX1Left->ComputeColumn(data.hydro->Vc);
+  columnX1Right->ComputeColumn(data.hydro->Vc);
+  columnX2Left->ComputeColumn(data.hydro->Vc);
+  columnX2Right->ComputeColumn(data.hydro->Vc);
+  columnX3Left->ComputeColumn(data.hydro->Vc);
+  columnX3Right->ComputeColumn(data.hydro->Vc);
+
+  IdefixArray3D<real> columnDensityLeft, columnDensityRight;
+  IdefixArray3D<real>::HostMirror columnDensityLeftHost, columnDensityRightHost;
+  // IDIR
+  columnDensityLeft = columnX1Left->GetColumn();
+  columnDensityRight = columnX1Right->GetColumn();
+  columnDensityLeftHost = Kokkos::create_mirror_view(columnDensityLeft);
+  columnDensityRightHost = Kokkos::create_mirror_view(columnDensityRight);
+  Kokkos::deep_copy(columnDensityLeftHost,columnDensityLeft);
+  Kokkos::deep_copy(columnDensityRightHost,columnDensityRight);
+
+  real errMax = 0.0;
+
+  for(int k = data.beg[KDIR]; k < data.end[KDIR] ; k++) {
+    for(int j = data.beg[JDIR]; j < data.end[JDIR] ; j++) {
+      for(int i = data.beg[IDIR]; i < data.end[IDIR] ; i++) {
+        real err = std::fabs(columnDensityLeftHost(k,j,i)-d.xr[IDIR](i));
+        if(err>errMax) errMax=err;
+        err = std::fabs(columnDensityRightHost(k,j,i)-(1-d.xl[IDIR](i)));
+        if(err>errMax) errMax=err;
+      }
+    }
+  }
+  idfx::cout << "Error on column density in IDIR=" << std::scientific << errMax << std::endl;
+  if(errMax>1e-14) {
+    IDEFIX_ERROR("Error above tolerance");
+  }
+  // JDIR
+  columnDensityLeft = columnX2Left->GetColumn();
+  columnDensityRight = columnX2Right->GetColumn();
+  columnDensityLeftHost = Kokkos::create_mirror_view(columnDensityLeft);
+  columnDensityRightHost = Kokkos::create_mirror_view(columnDensityRight);
+  Kokkos::deep_copy(columnDensityLeftHost,columnDensityLeft);
+  Kokkos::deep_copy(columnDensityRightHost,columnDensityRight);
+
+  errMax = 0.0;
+
+  for(int k = data.beg[KDIR]; k < data.end[KDIR] ; k++) {
+    for(int j = data.beg[JDIR]; j < data.end[JDIR] ; j++) {
+      for(int i = data.beg[IDIR]; i < data.end[IDIR] ; i++) {
+        real err = std::fabs(columnDensityLeftHost(k,j,i)-d.xr[JDIR](j));
+        if(err>errMax) errMax=err;
+        err = std::fabs(columnDensityRightHost(k,j,i)-(1-d.xl[JDIR](j)));
+        if(err>errMax) errMax=err;
+      }
+    }
+  }
+  idfx::cout << "Error on column density in JDIR=" << std::scientific << errMax << std::endl;
+  if(errMax>1e-14) {
+    IDEFIX_ERROR("Error above tolerance");
+  }
+  // KDIR
+  columnDensityLeft = columnX3Left->GetColumn();
+  columnDensityRight = columnX3Right->GetColumn();
+  columnDensityLeftHost = Kokkos::create_mirror_view(columnDensityLeft);
+  columnDensityRightHost = Kokkos::create_mirror_view(columnDensityRight);
+  Kokkos::deep_copy(columnDensityLeftHost,columnDensityLeft);
+  Kokkos::deep_copy(columnDensityRightHost,columnDensityRight);
+
+  errMax = 0.0;
+
+  for(int k = data.beg[KDIR]; k < data.end[KDIR] ; k++) {
+    for(int j = data.beg[JDIR]; j < data.end[JDIR] ; j++) {
+      for(int i = data.beg[IDIR]; i < data.end[IDIR] ; i++) {
+        real err = std::fabs(columnDensityLeftHost(k,j,i)-d.xr[KDIR](k));
+        if(err>errMax) errMax=err;
+        err = std::fabs(columnDensityRightHost(k,j,i)-(1-d.xl[KDIR](k)));
+        if(err>errMax) errMax=err;
+      }
+    }
+  }
+  idfx::cout << "Error on column density in KDIR=" << std::scientific << errMax << std::endl;
+  if(errMax>1e-14) {
+    IDEFIX_ERROR("Error above tolerance");
+  }
+
+}
+
+void InternalBoundary(Fluid<DefaultPhysics> * hydro, const real t) {
+  IdefixArray4D<real> Vc = hydro->Vc;
+  idefix_for("InternalBoundary",0,hydro->data->np_tot[KDIR],
+                                0,hydro->data->np_tot[JDIR],
+                                0,hydro->data->np_tot[IDIR],
+              KOKKOS_LAMBDA (int k, int j, int i) {
+                // Cancel any motion that could be happening
+                Vc(VX1,k,j,i) = 0.0;
+                Vc(VX2,k,j,i) = 0.0;
+                Vc(VX3,k,j,i) = 0.0;
+              });
+}
+
+
+// Initialisation routine. Can be used to allocate
+// Arrays or variables which are used later on
+Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output) {
+  output.EnrollAnalysis(&Analysis);
+  data.hydro->EnrollInternalBoundary(&InternalBoundary);
+
+  columnX1Left = new Column(IDIR, 1, RHO, &data);
+  columnX1Right = new Column(IDIR, -1, RHO, &data);
+  columnX2Left = new Column(JDIR, 1, RHO, &data);
+  columnX2Right = new Column(JDIR, -1, RHO, &data);
+  columnX3Left = new Column(KDIR, 1, RHO, &data);
+  columnX3Right = new Column(KDIR, -1, RHO, &data);
+  // Initialise the output file
+}
+
+// This routine initialize the flow
+// Note that data is on the device.
+// One can therefore define locally
+// a datahost and sync it, if needed
+void Setup::InitFlow(DataBlock &data) {
+    // Create a host copy
+    DataBlockHost d(data);
+
+
+    for(int k = 0; k < d.np_tot[KDIR] ; k++) {
+        for(int j = 0; j < d.np_tot[JDIR] ; j++) {
+            for(int i = 0; i < d.np_tot[IDIR] ; i++) {
+
+                d.Vc(RHO,k,j,i) = 1.0;
+                d.Vc(VX1,k,j,i) = ZERO_F;
+                d.Vc(PRS,k,j,i) = 1.0;
+
+            }
+        }
+    }
+
+    // Send it all, if needed
+    d.SyncToDevice();
+}

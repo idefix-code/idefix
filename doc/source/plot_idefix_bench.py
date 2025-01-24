49a56ee5b83d0822831bcb8f41859104bee5ea41
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2025-01-24 14:33:29 +0100

    Récupération de benchs pour H100; ajout de labels aux axes

--- doc/source/plot_idefix_bench.py
+++ doc/source/plot_idefix_bench.py
@@ -8,7 +8,10 @@ def do_plot(title, bench_file, gpumodel):
     select = [bench for bench in benches if bench['gpumodel'] == gpumodel][-1]
     xs = [r['nbgpu'] for r in select['results']]
     ys = [r['cell_updates'] for r in select['results']]
-    
-    plt.xscale("log")
+
+    plt.xscale("log", base=2)
     plt.plot(xs, ys)
+    plt.ylim(0,max(ys)*1.1)
+    plt.xlabel("Number of GPUs")
+    plt.ylabel("Performance (cells / second / GPU)")
     plt.title(title)

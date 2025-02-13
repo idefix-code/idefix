59a91d3cea976f8de290ee461a609cc865ee0fa2
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2025-02-13 14:07:17 +0100

    switch off heating terms when feedback is disabled (feedback assumes rho_d is negligible, so this makes sense anyway)

--- src/fluid/drag.cpp
+++ src/fluid/drag.cpp
@@ -36,16 +36,17 @@ void Drag::AddDragForce(const real dt) {
       for(int n = MX1 ; n < MX1+COMPONENTS ; n++) {
         real dv = VcDust(n,k,j,i) - VcGas(n,k,j,i);
         UcDust(n,k,j,i) -= dp*dv;
-        if(feedback) UcGas(n,k,j,i) += dp*dv;
-        #if HAVE_ENERGY == 1
-          // We add back the energy dissipated for the dust which is not accounted for
-          // (since there is no energy equation for dust grains)
+        if(feedback) {
+          UcGas(n,k,j,i) += dp*dv;
+          #if HAVE_ENERGY == 1
+            // We add back the energy dissipated for the dust which is not accounted for
+            // (since there is no energy equation for dust grains)
 
-          // TODO(GL): this should be disabled in the case of a true multifluid system where
-          // both fluids have a proper energy equation
-          UcGas(ENG,k,j,i) += dp*dv*VcDust(n,k,j,i);
-        #endif
-      }
+            // TODO(GL): this should be disabled in the case of a true multifluid system where
+            // both fluids have a proper energy equation
+            UcGas(ENG,k,j,i) += dp*dv*VcDust(n,k,j,i);
+          #endif
+        } // feedback
       // Cfl constraint
       real idt = gamma*VcGas(RHO,k,j,i);
       if(feedback) idt += gamma*VcDust(RHO,k,j,i);
@@ -167,7 +168,7 @@ void Drag::AddImplicitFluidMomentum(const real dt) {
 
           // TODO(GL): this should be disabled in the case of a true multifluid system where
           // both fluids have a proper energy equation
-          UcGas(ENG,k,j,i) -= dp*UcDust(n,k,j,i)/UcDust(RHO,k,j,i);
+          if(feedback) UcGas(ENG,k,j,i) -= dp*UcDust(n,k,j,i)/UcDust(RHO,k,j,i);
         #endif
       }
     });

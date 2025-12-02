309c975567e9b14319119135e42e535a603e640c
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2025-12-03 00:05:35 +0100

    refactor MPI  exchange routine.
    In the future, will allow exchange routines not to
    exchange normal field last active zone when shearing box is enabled.
    
    For this, the refactoring include an optional parameter "overwriteBXn"
    in each exchange direction

---
+++ src/mpi/exchanger.cpp
@@ -0,0 +1,298 @@
+// ***********************************************************************************
+// Idefix MHD astrophysical code
+// Copyright(C) Geoffroy R. J. Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
+// and other code contributors
+// Licensed under CeCILL 2.1 License, see COPYING for more information
+// ***********************************************************************************
+
+#include <vector>
+#include "exchanger.hpp"
+#include "idefix.hpp"
+#include "buffer.hpp"
+#include "grid.hpp"
+#include "arrays.hpp"
+
+void Exchanger::Init(
+              Grid *grid,
+              int direction,
+              std::vector<int> inputMap,
+              std::array<int, 3> nghost,
+              std::array<int, 3> nint,
+              bool inputHaveVs,
+              bool overwriteBXn) {
+  this->grid = grid;
+  this->direction = direction;
+  // Allocate mapVars on target and copy it from the input argument list
+  this->mapVars = idfx::ConvertVectorToIdefixArray(inputMap);
+  this->mapNVars = inputMap.size();
+  this->haveVs = inputHaveVs;
+
+  // Compute indices of arrays we will be working with
+  for(int dir = 0 ; dir < 3 ; dir++) {
+    this->nghost[dir] = nghost[dir];
+    this->nint[dir] = nint[dir];
+    this->ntot[dir] = nint[dir]+2*nghost[dir];
+    this->beg[dir] = nghost[dir];
+    this->end[dir] = nghost[dir]+nint[dir];
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Init exchange datasets
+  // Buffer size direction are for sends (i.e. buffer left is for a send from the left side)
+
+  // Make left buffer
+  // Init zone to the full domain
+  for(int dir = 0 ; dir < 3 ; dir++) {
+    if(dir < direction) {
+      boxRecv[faceLeft][dir][0] = 0;
+      boxRecv[faceLeft][dir][1] = ntot[dir];
+    } else if(dir > direction) {
+      boxRecv[faceLeft][dir][0] = beg[dir];
+      boxRecv[faceLeft][dir][1] = end[dir];
+    } else {
+      // dir == direction
+      boxRecv[faceLeft][dir][0] = 0;
+      boxRecv[faceLeft][dir][1] = nghost[IDIR];
+    }
+  }
+  // Copy all the boxes from boxRecvLeft
+  boxRecv[faceRight] = boxRecv[faceLeft];
+  boxSend = boxRecv;
+
+  // Adjust the indices for send and receive in the direction of interest
+  boxRecv[faceRight][direction][0] = end[direction];
+  boxRecv[faceRight][direction][1] = ntot[direction];
+
+  boxSend[faceLeft][direction][0] = beg[direction];
+  boxSend[faceLeft][direction][1] = beg[direction]+nghost[direction];
+
+  boxSend[faceRight][direction][0] = end[direction] - nghost[direction];
+  boxSend[faceRight][direction][1] = end[direction];
+
+  // Face-centered boxes
+
+  // Add one element in the field direction
+  for(int component = 0 ; component < 3 ; component++) {
+    // Init as centered boxes
+    boxSendVs[component] = boxSend;
+    boxRecvVs[component] = boxRecv;
+    const int normalDir = component;
+    if(component != direction) {
+      for(int face = 0 ; face <2 ; face++) {
+        boxSendVs[component][face][normalDir][1] += 1;
+        boxRecvVs[component][face][normalDir][1] += 1;
+      }
+    } else {
+      // component == direction
+      if(!overwriteBXn) boxSendVs[component][faceLeft][normalDir][0] += 1;
+      boxSendVs[component][faceLeft][normalDir][1] += 1;
+
+      if(!overwriteBXn) boxRecvVs[component][faceRight][normalDir][0] += 1;
+      boxRecvVs[component][faceRight][normalDir][1] += 1;
+    }
+  }
+
+  // Compute buffer sizes
+  for(int face=0 ; face < 2 ; face++) {
+    bufferSize[face] = mapNVars * Buffer::ComputeBoxSize(boxSend[face]);
+    if(haveVs) {
+      for(int component = 0 ; component <DIMENSIONS ; component++) {
+        bufferSize[face] += Buffer::ComputeBoxSize( boxSendVs[component][face] );
+      }
+    }
+  }
+
+  // allocate buffers
+  BufferSend[faceLeft] = Buffer(bufferSize[faceLeft]);
+  BufferRecv[faceRight] = Buffer(bufferSize[faceLeft]);
+
+  BufferSend[faceRight] = Buffer(bufferSize[faceRight]);
+  BufferRecv[faceLeft] = Buffer(bufferSize[faceRight]);
+
+  // Compute directions
+  MPI_Cart_shift(grid->CartComm,direction,1,&procRecv[faceLeft],&procSend[faceRight]);
+  MPI_Cart_shift(grid->CartComm,direction,-1,&procRecv[faceRight],&procSend[faceLeft]);
+
+  #ifdef MPI_PERSISTENT
+
+  // X1-dir exchanges
+  // We receive from procRecv, and we send to procSend
+
+  MPI_Send_init(BufferSend[faceRight].data(), bufferSize[faceRight], realMPI,
+            procSend[faceRight], thisInstance*100+10*direction,
+            grid->CartComm, &sendRequest[faceRight]);
+
+  MPI_Recv_init(BufferRecv[faceLeft].data(), bufferSize[faceRight], realMPI,
+            procRecv[faceLeft],thisInstance*100+10*direction,
+            grid->CartComm, &recvRequest[faceLeft]);
+
+  // Send to the left
+  // We receive from procRecv, and we send to procSend
+
+  MPI_Send_init(BufferSend[faceLeft].data(), bufferSize[faceLeft], realMPI,
+            procSend[faceLeft],thisInstance*100+10*direction+1,
+            grid->CartComm, &sendRequest[faceLeft]);
+
+  MPI_Recv_init(BufferRecvX1[faceRight].data(), bufferSizeX1[faceLeft], realMPI,
+            procRecv[faceRight], thisInstance*100+10*direction+1,
+            grid->CartComm, &recvRequest[faceRight]);
+
+  #endif // MPI_PERSISTENT
+
+  // say this instance is initialized.
+  isInitialized = true;
+
+  idfx::popRegion();
+}
+
+Exchanger::~Exchanger() {
+  idfx::pushRegion("Exchanger::~Exchanger");
+  if(isInitialized) {
+    // Properly clean up the mess
+    #ifdef MPI_PERSISTENT
+      for(int i=0 ; i< 2; i++) {
+        MPI_Request_free( &sendRequest[i]);
+        MPI_Request_free( &recvRequest[i]);
+      }
+    #endif
+    isInitialized = false;
+  }
+  idfx::popRegion();
+}
+
+void Exchanger::Exchange(IdefixArray4D<real> Vc, IdefixArray4D<real> Vs) {
+  idfx::pushRegion("Mpi::ExchangeX1");
+  // Load  the buffers with data
+  int ibeg,iend,jbeg,jend,kbeg,kend,offset,nx;
+  Buffer BufferLeft = BufferSend[faceLeft];
+  Buffer BufferRight = BufferSend[faceRight];
+  IdefixArray1D<int> map = this->mapVars;
+
+  bool recvRight = (procRecv[faceRight] != MPI_PROC_NULL);
+  bool recvLeft  = (procRecv[faceLeft] != MPI_PROC_NULL);
+
+  // If MPI Persistent, start receiving even before the buffers are filled
+  myTimer -= MPI_Wtime();
+  double tStart = MPI_Wtime();
+#ifdef MPI_PERSISTENT
+  MPI_Status sendStatus[2];
+  MPI_Status recvStatus[2];
+
+  MPI_Startall(2, recvRequestX1);
+  idfx::mpiCallsTimer += MPI_Wtime() - tStart;
+#endif
+  myTimer += MPI_Wtime();
+
+
+  BufferLeft.ResetPointer();
+  BufferRight.ResetPointer();
+
+  BufferLeft.Pack(Vc, map, boxSend[faceLeft]);
+
+  BufferRight.Pack(Vc, map, boxSend[faceRight]);
+  // Load face-centered field in the buffer
+  if(haveVs) {
+    for(int component = 0 ; component < DIMENSIONS ; component++) {
+      BufferLeft.Pack(Vs, component, boxSendVs[component][faceLeft]);
+      BufferRight.Pack(Vs, component, boxSendVs[component][faceRight]);
+    }
+  }
+
+  // Wait for completion before sending out everything
+  Kokkos::fence();
+  myTimer -= MPI_Wtime();
+  tStart = MPI_Wtime();
+#ifdef MPI_PERSISTENT
+  MPI_Startall(2, sendRequest);
+  // Wait for buffers to be received
+  MPI_Waitall(2,recvRequest,recvStatus);
+
+#else
+
+  #ifdef MPI_NON_BLOCKING
+  MPI_Status sendStatus[2];
+  MPI_Status recvStatus[2];
+  MPI_Request sendRequest[2];
+  MPI_Request recvRequest[2];
+
+  // We receive from procRecv, and we send to procSend
+
+  MPI_Isend(BufferSendX1[faceRight].data(), bufferSizeX1[faceRight], realMPI,
+            procSendX1[faceRight], 100, mygrid->CartComm, &sendRequest[0]);
+
+  MPI_Irecv(BufferRecvX1[faceLeft].data(), bufferSizeX1[faceRight], realMPI,
+            procRecvX1[faceLeft], 100, mygrid->CartComm, &recvRequest[0]);
+
+  // Send to the left
+  // We receive from procRecv, and we send to procSend
+
+  MPI_Isend(BufferSendX1[faceLeft].data(), bufferSizeX1[faceLeft], realMPI,
+            procSendX1[faceLeft], 101, mygrid->CartComm, &sendRequest[1]);
+
+  MPI_Irecv(BufferRecvX1[faceRight].data(), bufferSizeX1[faceLeft], realMPI,
+            procRecvX1[faceRight], 101, mygrid->CartComm, &recvRequest[1]);
+
+  // Wait for recv to complete (we don't care about the sends)
+  MPI_Waitall(2, recvRequest, recvStatus);
+
+  #else
+  MPI_Status status;
+  // Send to the right
+  // We receive from procRecv, and we send to procSend
+
+  MPI_Sendrecv(BufferSend[faceRight].data(), bufferSize[faceRight], realMPI,
+                procSend[faceRight], 100,
+                BufferRecv[faceLeft].data(), bufferSize[faceRight], realMPI,
+                procRecv[faceLeft], 100,
+                grid->CartComm, &status);
+
+  // Send to the left
+  // We receive from procRecv, and we send to procSend
+
+  MPI_Sendrecv(BufferSend[faceLeft].data(), bufferSize[faceLeft], realMPI,
+                procSend[faceLeft], 101,
+                BufferRecv[faceRight].data(), bufferSize[faceLeft], realMPI,
+                procRecv[faceRight], 101,
+                grid->CartComm, &status);
+  #endif
+#endif
+myTimer += MPI_Wtime();
+idfx::mpiCallsTimer += MPI_Wtime() - tStart;
+// Unpack
+BufferLeft=BufferRecv[faceLeft];
+BufferRight=BufferRecv[faceRight];
+
+BufferLeft.ResetPointer();
+BufferRight.ResetPointer();
+
+if(recvLeft) {
+    BufferLeft.Unpack(Vc, map, boxRecv[faceLeft]);
+  if(haveVs) {
+    for(int component = 0 ; component < DIMENSIONS ; component++) {
+      BufferLeft.Unpack(Vs, component, boxRecvVs[component][faceLeft]);
+    }
+  }
+}
+if(recvRight) {
+  BufferRight.Unpack(Vc, map, boxRecv[faceRight]);
+  // We fill the ghost zones
+  if(haveVs) {
+    for(int component = 0 ; component < DIMENSIONS ; component++) {
+      BufferRight.Unpack(Vs, component, boxRecvVs[component][faceRight]);
+    }
+  }
+}
+myTimer -= MPI_Wtime();
+#ifdef MPI_NON_BLOCKING
+  // Wait for the sends if they have not yet completed
+  MPI_Waitall(2, sendRequest, sendStatus);
+#endif
+
+#ifdef MPI_PERSISTENT
+  MPI_Waitall(2, sendRequest, sendStatus);
+#endif
+  myTimer += MPI_Wtime();
+  bytesSentOrReceived += 2*(bufferSize[faceLeft]+bufferSize[faceRight])*sizeof(real);
+
+  idfx::popRegion();
+}
---
+++ src/mpi/mpi.cpp
@@ -0,0 +1,257 @@
+// ***********************************************************************************
+// Idefix MHD astrophysical code
+// Copyright(C) Geoffroy R. J. Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
+// and other code contributors
+// Licensed under CeCILL 2.1 License, see COPYING for more information
+// ***********************************************************************************
+
+
+#include "mpi.hpp"
+#include <signal.h>
+#include <string>
+#include <chrono>   // NOLINT [build/c++11]
+#include <thread>  // NOLINT [build/c++11]
+#include <utility>
+#include <vector>
+#include "idefix.hpp"
+#include "dataBlock.hpp"
+
+
+#if defined(OPEN_MPI) && OPEN_MPI
+#include "mpi-ext.h"                // Needed for CUDA-aware check */
+#endif
+
+
+//#define MPI_NON_BLOCKING
+#define MPI_PERSISTENT
+
+// init the number of instances
+int Mpi::nInstances = 0;
+
+// MPI Routines exchange
+void Mpi::ExchangeAll() {
+  IDEFIX_ERROR("Not Implemented");
+}
+
+///
+/// Initialise an instance of the MPI class.
+/// @param grid: pointer to the grid object (needed to get the MPI neighbours)
+/// @param inputMap: 1st indices of inputVc which are to be exchanged (i.e, the list of variables)
+/// @param nghost: size of the ghost region in each direction
+/// @param nint: size of the internal region in each direction
+/// @param inputHaveVs: whether the instance should also treat face-centered variable
+///                     (optional, default false)
+///
+
+void Mpi::Init(Grid *grid, std::vector<int> inputMap,
+               int nghost[3], int nint[3],
+               bool inputHaveVs) {
+  idfx::pushRegion("Mpi::Init");
+
+  // increase the number of instances
+  nInstances++;
+  thisInstance=nInstances;
+
+  for(int dir=0; dir<3; dir++) {
+    exchanger[dir].Init(grid, dir, inputMap,
+                          {nghost[0], nghost[1], nghost[2]},
+                          {nint[0], nint[1], nint[2]},
+                          inputHaveVs);
+  }
+
+  isInitialized = true;
+  idfx::popRegion();
+}
+
+// Destructor (clean up persistent communication channels)
+Mpi::~Mpi() {
+  idfx::pushRegion("Mpi::~Mpi");
+  if(isInitialized) {
+    if(thisInstance==1) {
+      int bytesSentOrReceived = 0;
+      double myTimer = 0;
+      for(int dir=0; dir<3; dir++) {
+        bytesSentOrReceived += exchanger[dir].bytesSentOrReceived;
+        myTimer += exchanger[dir].myTimer;
+      }
+      idfx::cout << "Mpi(" << thisInstance << "): measured throughput is "
+                << bytesSentOrReceived/myTimer/1024.0/1024.0 << " MB/s" << std::endl;
+      idfx::cout << "Mpi(" << thisInstance << "): message sizes were " << std::endl;
+      idfx::cout << "        X1: "
+                 << exchanger[IDIR].bufferSize[0]*sizeof(real)/1024.0/1024.0
+                 << " MB" << std::endl;
+      idfx::cout << "        X2: "
+                 << exchanger[JDIR].bufferSize[0]*sizeof(real)/1024.0/1024.0
+                 << " MB" << std::endl;
+      idfx::cout << "        X3: "
+                 << exchanger[KDIR].bufferSize[0]*sizeof(real)/1024.0/1024.0
+                 << " MB" << std::endl;
+    }
+    isInitialized = false;
+  }
+  idfx::popRegion();
+}
+
+void Mpi::ExchangeX1(IdefixArray4D<real> Vc, IdefixArray4D<real> Vs) {
+  idfx::pushRegion("Mpi::ExchangeX1");
+
+  exchanger[IDIR].Exchange(Vc, Vs);
+  idfx::popRegion();
+}
+
+void Mpi::ExchangeX2(IdefixArray4D<real> Vc, IdefixArray4D<real> Vs) {
+  idfx::pushRegion("Mpi::ExchangeX2");
+  exchanger[JDIR].Exchange(Vc, Vs);
+  idfx::popRegion();
+}
+
+void Mpi::ExchangeX3(IdefixArray4D<real> Vc, IdefixArray4D<real> Vs) {
+  idfx::pushRegion("Mpi::ExchangeX3");
+  exchanger[KDIR].Exchange(Vc, Vs);
+  idfx::popRegion();
+}
+
+
+void Mpi::CheckConfig() {
+  idfx::pushRegion("Mpi::CheckConfig");
+  // compile time check
+  #ifdef KOKKOS_ENABLE_CUDA
+    #if defined(MPIX_CUDA_AWARE_SUPPORT) && !MPIX_CUDA_AWARE_SUPPORT
+      #error Your MPI library is not CUDA Aware (check Idefix requirements).
+    #endif
+  #endif /* MPIX_CUDA_AWARE_SUPPORT */
+
+  // Run-time check that we can do a reduce on device arrays
+  IdefixArray1D<int64_t> src("MPIChecksrc",1);
+  IdefixArray1D<int64_t>::HostMirror srcHost = Kokkos::create_mirror_view(src);
+
+  if(idfx::prank == 0) {
+    srcHost(0) = 0;
+    Kokkos::deep_copy(src, srcHost);
+  }
+
+  if(idfx::psize > 1) {
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+    // Capture segfaults
+    struct sigaction newHandler;
+    struct sigaction oldHandler;
+    memset(&newHandler, 0, sizeof(newHandler));
+    newHandler.sa_flags = SA_SIGINFO;
+    newHandler.sa_sigaction = Mpi::SigErrorHandler;
+    sigaction(SIGSEGV, &newHandler, &oldHandler);
+    try {
+      // We next circulate the info round-robin accross all the nodes to check that
+      // MPI can exchange buffers in idefix arrays
+
+      MPI_Status status;
+      int ierrSend, ierrRecv;
+      if(idfx::prank == 0) {
+        ierrSend = MPI_Send(src.data(), 1, MPI_INT64_T, idfx::prank+1, 1, MPI_COMM_WORLD);
+        ierrRecv = MPI_Recv(src.data(), 1, MPI_INT64_T, idfx::psize-1, 1, MPI_COMM_WORLD, &status);
+      } else {
+        ierrRecv = MPI_Recv(src.data(), 1, MPI_INT64_T, idfx::prank-1, 1, MPI_COMM_WORLD, &status);
+        // Add our own rank to the data
+        Kokkos::deep_copy(srcHost, src);
+        srcHost(0) += idfx::prank;
+        Kokkos::deep_copy(src, srcHost);
+        ierrSend = MPI_Send(src.data(), 1, MPI_INT64_T, (idfx::prank+1)%idfx::psize, 1,
+                                                        MPI_COMM_WORLD);
+      }
+
+      if(ierrSend != 0) {
+        char MPImsg[MPI_MAX_ERROR_STRING];
+        int MPImsgLen;
+        MPI_Error_string(ierrSend, MPImsg, &MPImsgLen);
+        throw std::runtime_error(std::string(MPImsg, MPImsgLen));
+      }
+      if(ierrRecv != 0) {
+        char MPImsg[MPI_MAX_ERROR_STRING];
+        int MPImsgLen;
+        MPI_Error_string(ierrSend, MPImsg, &MPImsgLen);
+        throw std::runtime_error(std::string(MPImsg, MPImsgLen));
+      }
+    } catch(std::exception &e) {
+      std::stringstream errmsg;
+      errmsg << "Your MPI library is unable to perform Send/Recv on Idefix arrays.";
+      errmsg << std::endl;
+      #ifdef KOKKOS_ENABLE_CUDA
+        errmsg << "Check that your MPI library is CUDA aware." << std::endl;
+      #elif defined(KOKKOS_ENABLE_HIP)
+        errmsg << "Check that your MPI library is RocM aware." << std::endl;
+      #else
+        errmsg << "Check your MPI library configuration." << std::endl;
+      #endif
+      errmsg << "Error: " << e.what() << std::endl;
+      IDEFIX_ERROR(errmsg);
+    }
+    // Restore old handlers
+    sigaction(SIGSEGV, &oldHandler, NULL );
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
+  }
+
+  // Check that we have the proper end result
+  Kokkos::deep_copy(srcHost, src);
+  int64_t size = static_cast<int64_t>(idfx::psize);
+  int64_t rank = static_cast<int64_t>(idfx::prank);
+  int64_t result = rank == 0 ? size*(size-1)/2 : rank*(rank+1)/2;
+
+  if(srcHost(0) != result) {
+    idfx::cout << "got " << srcHost(0) << " expected " << result << std::endl;
+    std::stringstream errmsg;
+    errmsg << "Your MPI library managed to perform MPI exchanges on Idefix Arrays, but the result ";
+    errmsg << "is incorrect. " << std::endl;
+    errmsg << "Check your MPI library configuration." << std::endl;
+    IDEFIX_ERROR(errmsg);
+  }
+  idfx::popRegion();
+}
+
+void Mpi::SigErrorHandler(int nSignum, siginfo_t* si, void* vcontext) {
+  std::stringstream errmsg;
+  errmsg << "A segmentation fault was triggered while attempting to test your MPI library.";
+  errmsg << std::endl;
+  errmsg << "Your MPI library is unable to perform reductions on Idefix arrays.";
+  errmsg << std::endl;
+  #ifdef KOKKOS_ENABLE_CUDA
+    errmsg << "Check that your MPI library is CUDA aware." << std::endl;
+  #elif defined(KOKKOS_ENABLE_HIP)
+    errmsg << "Check that your MPI library is RocM aware." << std::endl;
+  #else
+    errmsg << "Check your MPI library configuration." << std::endl;
+  #endif
+  IDEFIX_ERROR(errmsg);
+}
+
+// This routine check that all of the processes are synced.
+// Returns true if this is the case, false otherwise
+
+bool Mpi::CheckSync(real timeout) {
+  // If no parallelism, then we're in sync!
+  if(idfx::psize == 1) return(true);
+
+  int send = idfx::prank;
+  int recv = 0;
+  MPI_Request request;
+
+  MPI_Iallreduce(&send, &recv, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD, &request);
+
+  double start = MPI_Wtime();
+  int flag = 0;
+  MPI_Status status;
+
+  while((MPI_Wtime()-start < timeout) && !flag) {
+    MPI_Test(&request, &flag, &status);
+    // sleep for 10 ms
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+  if(!flag) {
+    // We did not managed to do an allreduce, so this is a failure.
+    return(false);
+  }
+  if(recv != idfx::psize*(idfx::psize-1)/2) {
+    IDEFIX_ERROR("wrong result for synchronisation");
+  }
+
+  return(true);
+}

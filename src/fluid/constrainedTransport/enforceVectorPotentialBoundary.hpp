d83d694bf2f3104c0a16cf15bdf057e71fd2cf17
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2026-01-21 10:21:06 +0100

    fix incorrect subviews when using vector_potential in 2.5D (#362)
    * fix incorrect subviews when using vector_potential in 2.5D

--- src/fluid/constrainedTransport/enforceVectorPotentialBoundary.hpp
+++ src/fluid/constrainedTransport/enforceVectorPotentialBoundary.hpp
@@ -13,23 +13,31 @@ template<typename Phys>
 void ConstrainedTransport<Phys>::EnforceVectorPotentialBoundary(IdefixArray4D<real> &Vein) {
   idfx::pushRegion("Emf::EnforceVectorPotentialBoundary");
 
-  auto Ax1 = Kokkos::subview(Vein, IDIR, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
-  auto Ax2 = Kokkos::subview(Vein, JDIR, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
-  auto Ax3 = Kokkos::subview(Vein, KDIR, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
 
-  if(this->hydro->haveAxis) {
-    this->hydro->boundary->axis->RegularizeEMFs(Ax1, Ax2, Ax3);
-  }
+  IdefixArray3D<real> Ax1, Ax2, Ax3;
 
-  #ifdef ENFORCE_EMF_CONSISTENCY
-    #ifdef WITH_MPI
-      // This average the vector potential at the domain surface with immediate neighbours
-      // to ensure the vector potentials exactly match
-
-      this->ExchangeAll(Ax1, Ax2, Ax3);
+  #ifdef EVOLVE_VECTOR_POTENTIAL
+    #if DIMENSIONS == 3
+      Ax1 = Kokkos::subview(Vein, AX1e, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
+      Ax2 = Kokkos::subview(Vein, AX2e, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
     #endif
-    EnforceEMFBoundaryPeriodic(Ax1, Ax2, Ax3);
-  #endif
+    Ax3 = Kokkos::subview(Vein, AX3e, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
+
+
+    if(this->hydro->haveAxis) {
+      this->hydro->boundary->axis->RegularizeEMFs(Ax1, Ax2, Ax3);
+    }
+
+    #ifdef ENFORCE_EMF_CONSISTENCY
+      #ifdef WITH_MPI
+        // This average the vector potential at the domain surface with immediate neighbours
+        // to ensure the vector potentials exactly match
+
+        this->ExchangeAll(Ax1, Ax2, Ax3);
+      #endif
+      EnforceEMFBoundaryPeriodic(Ax1, Ax2, Ax3);
+    #endif
+  #endif // EVOLVE_VECTOR_POTENTIAL
 
   idfx::popRegion();
 }

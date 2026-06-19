56a570b9cd76b54081ca368dbce088a9652542a8
Sebastien Valat <sebastien.valat@univ-grenoble-alpes.fr>
2026-06-19 16:48:37 +0200

    refactor: use Buffer and BoundingBox for the MPI communications of ConstrainedTransport (step 1)

--- src/fluid/constrainedTransport/EMFexchange.hpp
+++ src/fluid/constrainedTransport/EMFexchange.hpp
@@ -8,6 +8,8 @@
 #ifndef FLUID_CONSTRAINEDTRANSPORT_EMFEXCHANGE_HPP_
 #define FLUID_CONSTRAINEDTRANSPORT_EMFEXCHANGE_HPP_
 
+#include <cassert>
+
 #include "constrainedTransport.hpp"
 #include "fluid.hpp"
 #include "dataBlock.hpp"
@@ -36,14 +38,19 @@ void ConstrainedTransport<Phys>::ExchangeX1(IdefixArray3D<real> ey, IdefixArray3
 
   IdefixArray1D<real> BufferLeft=BufferSendX1[faceLeft];
   IdefixArray1D<real> BufferRight=BufferSendX1[faceRight];
+  Buffer BufferLeftNew=BufferSendX1New[faceLeft];
+  Buffer BufferRightNew=BufferSendX1New[faceRight];
 
 
   // If MPI Persistent, start receiving even before the buffers are filled
   MPI_Status sendStatus[2];
   MPI_Status recvStatus[2];
+  MPI_Status sendStatusNew[2];
+  MPI_Status recvStatusNew[2];
 
   double tStart = MPI_Wtime();
   MPI_SAFE_CALL(MPI_Startall(2, recvRequestX1));
+  MPI_SAFE_CALL(MPI_Startall(2, recvRequestX1New));
   idfx::mpiCallsTimer += MPI_Wtime() - tStart;
 
   BoundaryType lbound = data->lbound[IDIR];
@@ -59,50 +66,97 @@ void ConstrainedTransport<Phys>::ExchangeX1(IdefixArray3D<real> ey, IdefixArray3
   kend   = data->end[KDIR];
   nz     = kend - kbeg;
 
+  // Create the base box to be patched by the ops
+  BoundingBox baseBox;
+  baseBox[IDIR][0] = data->beg[IDIR];
+  baseBox[IDIR][1] = data->end[IDIR];
+  baseBox[JDIR][0] = data->beg[JDIR];
+  baseBox[JDIR][1] = data->end[JDIR];
+  baseBox[KDIR][0] = data->beg[KDIR];
+  baseBox[KDIR][1] = data->end[KDIR];
+
+  //extend by one the end on jdir && take the ghost on i
+  BoundingBox sendBoxEz = baseBox;
+  sendBoxEz[JDIR][1] += 1;
+  sendBoxEz[IDIR][0] = iright;
+  sendBoxEz[IDIR][1] = iright + 1;
+  BufferRightNew.Pack(ez, sendBoxEz);
   idefix_for("LoadBufferX1Emfz",kbeg,kend,jbeg,jend+1,
     KOKKOS_LAMBDA (int k, int j) {
       BufferRight( (j-jbeg) + (k-kbeg)*(ny+1) ) = ez(k,j,iright);
+      assert(BufferRightNew.getArray()((j-jbeg) + (k-kbeg)*(ny+1)) == ez(k,j,iright));
     }
   );
   #if DIMENSIONS == 3
   int Vsindex = (ny+1)*nz;
 
+  //extend by one the end on kdir && take the ghost on i
+  BoundingBox sendBoxEy = baseBox;
+  sendBoxEy[KDIR][1] += 1;
+  sendBoxEy[IDIR][0] = iright;
+  sendBoxEy[IDIR][1] = iright + 1;
+  BufferRightNew.Pack(ez, sendBoxEy);
   idefix_for("LoadBufferX1Emfy",kbeg,kend+1,jbeg,jend,
     KOKKOS_LAMBDA (int k, int j) {
       BufferRight( (j-jbeg) + (k-kbeg)*ny + Vsindex ) = ey(k,j,iright);
+      assert(BufferRightNew.getArray()( (j-jbeg) + (k-kbeg)*ny + Vsindex ) == ey(k,j,iright));
     }
   );
   #endif
+
+  assert(BufferRight == BufferRightNew.getArray());
 
   // Wait for completion before sending out everything
   Kokkos::fence();
 
   tStart = MPI_Wtime();
   MPI_SAFE_CALL(MPI_Startall(2, sendRequestX1));
+  MPI_SAFE_CALL(MPI_Startall(2, sendRequestX1New));
   // Wait for buffers to be received
 
   MPI_Waitall(2,recvRequestX1,recvStatus);
+  MPI_Waitall(2,recvRequestX1New,recvStatusNew);
   MPI_Waitall(2, sendRequestX1, sendStatus);
+  MPI_Waitall(2, sendRequestX1New, sendStatusNew);
   idfx::mpiCallsTimer += MPI_Wtime() - tStart;
 
   // Unpack
   BufferLeft=BufferRecvX1[faceLeft];
   BufferRight=BufferRecvX1[faceRight];
+  BufferLeftNew=BufferRecvX1New[faceLeft];
+  BufferRightNew=BufferRecvX1New[faceRight];
+
+
+
+  assert(BufferLeft == BufferLeftNew.getArray());
 
   // Erase the emf with the one coming from the left process
-
+  //extend by one the end on jdir && take the ghost revc zone on i
+  BoundingBox recvBoxEz = baseBox;
+  recvBoxEz[JDIR][1] += 1;
+  recvBoxEz[IDIR][0] = ileft;
+  recvBoxEz[IDIR][1] = ileft + 1;
+  BufferLeftNew.Unpack(ez, recvBoxEz);
   idefix_for("StoreBufferX1Emfz",kbeg,kend,jbeg,jend+1,
     KOKKOS_LAMBDA (int k, int j) {
       if(lbound == internal || lbound == periodic) {
+        assert(ez(k,j,ileft) == BufferLeft( (j-jbeg) + (k-kbeg)*(ny+1)));
         ez(k,j,ileft) = BufferLeft( (j-jbeg) + (k-kbeg)*(ny+1));
       }
     });
 
   #if DIMENSIONS == 3
   Vsindex = (ny+1)*nz;
+  //extend by one the end on kdir && take the ghost revc zone on i
+  BoundingBox recvBoxEy = baseBox;
+  recvBoxEy[KDIR][1] += 1;
+  recvBoxEy[IDIR][0] = ileft;
+  recvBoxEy[IDIR][1] = ileft + 1;
+  BufferLeftNew.Unpack(ey, recvBoxEy);
   idefix_for("StoreBufferX1Emfy",kbeg,kend+1,jbeg,jend,
     KOKKOS_LAMBDA (int k, int j) {
       if(lbound == internal || lbound == periodic) {
+        assert(ey(k,j,ileft) == BufferLeft( (j-jbeg) + (k-kbeg)*ny +Vsindex));
         ey(k,j,ileft) = BufferLeft( (j-jbeg) + (k-kbeg)*ny +Vsindex);
       }
     });
@@ -123,12 +177,17 @@ void ConstrainedTransport<Phys>::ExchangeX2(IdefixArray3D<real> ex, IdefixArray3
   [[maybe_unused]] int nz;
   IdefixArray1D<real> BufferLeft=BufferSendX2[faceLeft];
   IdefixArray1D<real> BufferRight=BufferSendX2[faceRight];
+  Buffer BufferLeftNew=BufferSendX2New[faceLeft];
+  Buffer BufferRightNew=BufferSendX2New[faceRight];
 
   // If MPI Persistent, start receiving even before the buffers are filled
   double tStart = MPI_Wtime();
   MPI_Status sendStatus[2];
   MPI_Status recvStatus[2];
+  MPI_Status sendStatusNew[2];
+  MPI_Status recvStatusNew[2];
   MPI_SAFE_CALL(MPI_Startall(2, recvRequestX2));
+  MPI_SAFE_CALL(MPI_Startall(2, recvRequestX2New));
   idfx::mpiCallsTimer += MPI_Wtime() - tStart;
 
   BoundaryType lbound = data->lbound[JDIR];
@@ -144,47 +203,93 @@ void ConstrainedTransport<Phys>::ExchangeX2(IdefixArray3D<real> ex, IdefixArray3
   kend   = data->end[KDIR];
   nz     = kend - kbeg;
 
+  // Create the base box to be patched by the ops
+  BoundingBox baseBox;
+  baseBox[IDIR][0] = data->beg[IDIR];
+  baseBox[IDIR][1] = data->end[IDIR];
+  baseBox[JDIR][0] = data->beg[JDIR];
+  baseBox[JDIR][1] = data->end[JDIR];
+  baseBox[KDIR][0] = data->beg[KDIR];
+  baseBox[KDIR][1] = data->end[KDIR];
+
+  //extend by one the end on idir && take the ghost on j
+  BoundingBox sendBoxEz = baseBox;
+  sendBoxEz[IDIR][1] += 1;
+  sendBoxEz[JDIR][0] = jright;
+  sendBoxEz[JDIR][1] = jright + 1;
+  BufferRightNew.Pack(ez, sendBoxEz);
   idefix_for("LoadBufferX2Emfz",kbeg,kend,ibeg,iend+1,
     KOKKOS_LAMBDA (int k, int i) {
       BufferRight( (i-ibeg) + (k-kbeg)*(nx+1) ) = ez(k,jright,i);
+      assert(BufferRightNew.getArray()( (i-ibeg) + (k-kbeg)*(nx+1) ) == ez(k,jright,i));
     }
   );
   #if DIMENSIONS == 3
   int Vsindex = (nx+1)*nz;
 
+  //extend by one the end on idir && take the ghost on j
+  BoundingBox sendBoxEx = baseBox;
+  sendBoxEx[KDIR][1] += 1;
+  sendBoxEx[JDIR][0] = jright;
+  sendBoxEx[JDIR][1] = jright + 1;
+  BufferRightNew.Pack(ex, sendBoxEx);
   idefix_for("LoadBufferX1Emfx",kbeg,kend+1,ibeg,iend,
     KOKKOS_LAMBDA (int k, int i) {
       BufferRight( (i-ibeg) + (k-kbeg)*nx + Vsindex ) = ex(k,jright,i);
+      assert(BufferRightNew.getArray()( (i-ibeg) + (k-kbeg)*nx + Vsindex ) == ex(k,jright,i));
     }
   );
   #endif
+
+  assert(BufferRightNew.getArray() == BufferRight);
 
   // Wait for completion before sending out everything
   Kokkos::fence();
 
   tStart = MPI_Wtime();
   MPI_SAFE_CALL(MPI_Startall(2, sendRequestX2));
+  MPI_SAFE_CALL(MPI_Startall(2, sendRequestX2New));
   // Wait for buffers to be received
   MPI_Waitall(2,recvRequestX2,recvStatus);
+  MPI_Waitall(2,recvRequestX2New,recvStatus);
   MPI_Waitall(2, sendRequestX2, sendStatus);
+  MPI_Waitall(2, sendRequestX2New, sendStatus);
   idfx::mpiCallsTimer += MPI_Wtime() - tStart;
 
   // Unpack
   BufferLeft=BufferRecvX2[faceLeft];
   BufferRight=BufferRecvX2[faceRight];
+  BufferLeftNew=BufferRecvX2New[faceLeft];
+  BufferRightNew=BufferRecvX2New[faceRight];
+
+  assert(BufferLeftNew.getArray() == BufferLeft);
 
   // We average the edge emfs zones
+  //extend by one the end on idir && take the ghost revc zone on j
+  BoundingBox recvBoxEz = baseBox;
+  recvBoxEz[IDIR][1] += 1;
+  recvBoxEz[JDIR][0] = jleft;
+  recvBoxEz[JDIR][1] = jleft + 1;
+  BufferLeftNew.Unpack(ez, recvBoxEz);
   idefix_for("StoreBufferX2Emfz",kbeg,kend,ibeg,iend+1,
     KOKKOS_LAMBDA (int k, int i) {
       if(lbound == internal || lbound == periodic) {
+        assert(ez(k,jleft,i) == BufferLeft( (i-ibeg) + (k-kbeg)*(nx+1) ));
         ez(k,jleft,i) = BufferLeft( (i-ibeg) + (k-kbeg)*(nx+1) );
       }
     });
   #if DIMENSIONS == 3
   Vsindex = (nx+1)*nz;
+  //extend by one the end on kdir && take the ghost revc zone on j
+  BoundingBox recvBoxEx = baseBox;
+  recvBoxEx[KDIR][1] += 1;
+  recvBoxEx[JDIR][0] = jleft;
+  recvBoxEx[JDIR][1] = jleft + 1;
+  BufferLeftNew.Unpack(ex, recvBoxEx);
   idefix_for("StoreBufferX1Emfy",kbeg,kend+1,ibeg,iend,
     KOKKOS_LAMBDA (int k, int i) {
       if(lbound == internal || lbound == periodic) {
+        assert(ex(k,jleft,i) == BufferLeft( (i-ibeg) + (k-kbeg)*nx +Vsindex));
         ex(k,jleft,i) = BufferLeft( (i-ibeg) + (k-kbeg)*nx +Vsindex);
       }
     });
@@ -205,6 +310,8 @@ void ConstrainedTransport<Phys>::ExchangeX3(IdefixArray3D<real> ex, IdefixArray3
   int nx,ny;
   IdefixArray1D<real> BufferLeft=BufferSendX3[faceLeft];
   IdefixArray1D<real> BufferRight=BufferSendX3[faceRight];
+  Buffer BufferLeftNew=BufferSendX3New[faceLeft];
+  Buffer BufferRightNew=BufferSendX3New[faceRight];
 
   int Vsindex = 0;
 
@@ -213,7 +320,10 @@ void ConstrainedTransport<Phys>::ExchangeX3(IdefixArray3D<real> ex, IdefixArray3
   double tStart = MPI_Wtime();
   MPI_Status sendStatus[2];
   MPI_Status recvStatus[2];
+  MPI_Status sendStatusNew[2];
+  MPI_Status recvStatusNew[2];
   MPI_SAFE_CALL(MPI_Startall(2, recvRequestX3));
+  MPI_SAFE_CALL(MPI_Startall(2, recvRequestX3New));
   idfx::mpiCallsTimer += MPI_Wtime() - tStart;
 
   BoundaryType lbound = data->lbound[KDIR];
@@ -231,45 +341,91 @@ void ConstrainedTransport<Phys>::ExchangeX3(IdefixArray3D<real> ex, IdefixArray3
   kleft   = data->beg[KDIR];
   kright = data->end[KDIR];
 
+  // Create the base box to be patched by the ops
+  BoundingBox baseBox;
+  baseBox[IDIR][0] = data->beg[IDIR];
+  baseBox[IDIR][1] = data->end[IDIR];
+  baseBox[JDIR][0] = data->beg[JDIR];
+  baseBox[JDIR][1] = data->end[JDIR];
+  baseBox[KDIR][0] = data->beg[KDIR];
+  baseBox[KDIR][1] = data->end[KDIR];
+
+  //extend by one the end on jdir && take the ghost on k
+  BoundingBox sendBoxEz = baseBox;
+  sendBoxEz[JDIR][1] += 1;
+  sendBoxEz[KDIR][0] = kright;
+  sendBoxEz[KDIR][1] = kright + 1;
+  BufferRightNew.Pack(ez, sendBoxEz);
   idefix_for("LoadBufferX3Emfx",jbeg,jend+1,ibeg,iend,
     KOKKOS_LAMBDA (int j, int i) {
       BufferRight( (i-ibeg) + (j-jbeg)*nx ) = ex(kright,j,i);
+      assert(BufferRightNew.getArray()( (i-ibeg) + (j-jbeg)*nx ) == ex(kright,j,i));
     }
   );
   Vsindex = nx*(ny+1);
 
+  //extend by one the end on idir && take the ghost on k
+  BoundingBox sendBoxEy = baseBox;
+  sendBoxEy[IDIR][1] += 1;
+  sendBoxEy[KDIR][0] = kright;
+  sendBoxEy[KDIR][1] = kright + 1;
+  BufferRightNew.Pack(ey, sendBoxEy);
   idefix_for("LoadBufferX3Emfy",jbeg,jend,ibeg,iend+1,
     KOKKOS_LAMBDA (int j, int i) {
       BufferRight( (i-ibeg) + (j-jbeg)*(nx+1) + Vsindex ) = ey(kright,j,i);
+      assert(BufferRightNew.getArray()( (i-ibeg) + (j-jbeg)*(nx+1) + Vsindex ) == ey(kright,j,i));
     }
   );
+
+  assert(BufferRightNew.getArray() == BufferRight);
 
   // Wait for completion before sending out everything
   Kokkos::fence();
 
   tStart = MPI_Wtime();
   MPI_SAFE_CALL(MPI_Startall(2, sendRequestX3));
+  MPI_SAFE_CALL(MPI_Startall(2, sendRequestX3New));
   // Wait for buffers to be received
   MPI_Waitall(2,recvRequestX3,recvStatus);
+  MPI_Waitall(2,recvRequestX3New,recvStatusNew);
   MPI_Waitall(2, sendRequestX3, sendStatus);
+  MPI_Waitall(2, sendRequestX3New, sendStatusNew);
   idfx::mpiCallsTimer += MPI_Wtime() - tStart;
 
   // Unpack
   BufferLeft=BufferRecvX3[faceLeft];
   BufferRight=BufferRecvX3[faceRight];
+  BufferLeftNew=BufferRecvX3New[faceLeft];
+  BufferRightNew=BufferRecvX3New[faceRight];
+
+  assert(BufferLeftNew.getArray() == BufferLeft);
 
   // We average the edge emfs zones
+  //extend by one the end on jdir && take the ghost revc zone on k
+  BoundingBox recvBoxEx = baseBox;
+  recvBoxEx[JDIR][1] += 1;
+  recvBoxEx[KDIR][0] = kleft;
+  recvBoxEx[KDIR][1] = kleft + 1;
+  BufferLeftNew.Unpack(ex, recvBoxEx);
   idefix_for("StoreBufferX3Emfx",jbeg,jend+1,ibeg,iend,
     KOKKOS_LAMBDA (int j, int i) {
       if(lbound == internal || lbound == periodic) {
+        assert(ex(kleft,j,i) == BufferLeft( (i-ibeg) + (j-jbeg)*nx ));
         ex(kleft,j,i) = BufferLeft( (i-ibeg) + (j-jbeg)*nx );
       }
     });
 
   Vsindex = nx*(ny+1);
+  //extend by one the end on idir && take the ghost revc zone on k
+  BoundingBox recvBoxEy = baseBox;
+  recvBoxEy[IDIR][1] += 1;
+  recvBoxEy[KDIR][0] = kleft;
+  recvBoxEy[KDIR][1] = kleft + 1;
+  BufferLeftNew.Unpack(ey, recvBoxEy);
   idefix_for("StoreBufferX3Emfy",jbeg,jend,ibeg,iend+1,
     KOKKOS_LAMBDA (int j, int i) {
       if(lbound == internal || lbound == periodic) {
+        assert(ey(kleft,j,i) == BufferLeft( (i-ibeg) + (j-jbeg)*(nx+1) + Vsindex ));
         ey(kleft,j,i) = BufferLeft( (i-ibeg) + (j-jbeg)*(nx+1) + Vsindex );
       }
     });
--- src/fluid/constrainedTransport/constrainedTransport.hpp
+++ src/fluid/constrainedTransport/constrainedTransport.hpp
@@ -11,6 +11,7 @@
 #include "idefix.hpp"
 #include "input.hpp"
 #include "riemannSolver.hpp"
+#include "buffer.hpp"
 
 // Forward declarations
 #include "physics.hpp"
@@ -128,6 +129,12 @@ class ConstrainedTransport {
   IdefixArray1D<real> BufferRecvX1[2];
   IdefixArray1D<real> BufferRecvX2[2];
   IdefixArray1D<real> BufferRecvX3[2];
+  Buffer BufferSendX1New[2];
+  Buffer BufferSendX2New[2];
+  Buffer BufferSendX3New[2];
+  Buffer BufferRecvX1New[2];
+  Buffer BufferRecvX2New[2];
+  Buffer BufferRecvX3New[2];
 
   IdefixArray1D<int>  mapVars;
 
@@ -142,6 +149,12 @@ class ConstrainedTransport {
   MPI_Request recvRequestX1[2];
   MPI_Request recvRequestX2[2];
   MPI_Request recvRequestX3[2];
+  MPI_Request sendRequestX1New[2];
+  MPI_Request sendRequestX2New[2];
+  MPI_Request sendRequestX3New[2];
+  MPI_Request recvRequestX1New[2];
+  MPI_Request recvRequestX2New[2];
+  MPI_Request recvRequestX3New[2];
 
   Kokkos::Timer timer;    // Internal MPI timer
 #endif
@@ -291,6 +304,11 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
   BufferSendX1[faceLeft ] = IdefixArray1D<real>("EmfSendX1Left", bufferSizeX1);
   BufferSendX1[faceRight] = IdefixArray1D<real>("EmfSendX1Right",bufferSizeX1);
 
+  BufferRecvX1New[faceLeft ] = Buffer(bufferSizeX1);
+  BufferRecvX1New[faceRight] = Buffer(bufferSizeX1);
+  BufferSendX1New[faceLeft ] = Buffer(bufferSizeX1);
+  BufferSendX1New[faceRight] = Buffer(bufferSizeX1);
+
   // Number of cells in X2 boundary condition (only required when problem >2D):
 #if DIMENSIONS >= 2
 
@@ -306,6 +324,11 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
   BufferSendX2[faceLeft ] = IdefixArray1D<real>("EmfSendX2Left", bufferSizeX2);
   BufferSendX2[faceRight] = IdefixArray1D<real>("EmfSendX2Right",bufferSizeX2);
 
+  BufferRecvX2New[faceLeft ] = Buffer(bufferSizeX2);
+  BufferRecvX2New[faceRight] = Buffer(bufferSizeX2);
+  BufferSendX2New[faceLeft ] = Buffer(bufferSizeX2);
+  BufferSendX2New[faceRight] = Buffer(bufferSizeX2);
+
 #endif
 // Number of cells in X3 boundary condition (only required when problem is 3D):
 #if DIMENSIONS ==3
@@ -318,6 +341,11 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
   BufferRecvX3[faceRight] = IdefixArray1D<real>("EmfRecvX3Right",bufferSizeX3);
   BufferSendX3[faceLeft ] = IdefixArray1D<real>("EmfSendX3Left", bufferSizeX3);
   BufferSendX3[faceRight] = IdefixArray1D<real>("EmfSendX3Right",bufferSizeX3);
+
+  BufferRecvX3New[faceLeft ] = Buffer(bufferSizeX3);
+  BufferRecvX3New[faceRight] = Buffer(bufferSizeX3);
+  BufferSendX3New[faceLeft ] = Buffer(bufferSizeX3);
+  BufferSendX3New[faceRight] = Buffer(bufferSizeX3);
 #endif // DIMENSIONS
 
   // Init persistent MPI communications
@@ -332,9 +360,15 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
 
   MPI_SAFE_CALL(MPI_Send_init(BufferSendX1[faceRight].data(), bufferSizeX1, realMPI, procSend, 100,
                 mygrid->CartComm, &sendRequestX1[faceRight]));
+  MPI_SAFE_CALL(MPI_Send_init(BufferSendX1New[faceRight].data(), bufferSizeX1, realMPI, procSend,
+                1100,
+                mygrid->CartComm, &sendRequestX1New[faceRight]));
 
   MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX1[faceLeft].data(), bufferSizeX1, realMPI, procRecv, 100,
                 mygrid->CartComm, &recvRequestX1[faceLeft]));
+  MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX1New[faceLeft].data(), bufferSizeX1, realMPI, procRecv,
+                1100,
+                mygrid->CartComm, &recvRequestX1New[faceLeft]));
 
   // Send to the left
   // We receive from procRecv, and we send to procSend
@@ -345,9 +379,15 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
 
   MPI_SAFE_CALL(MPI_Send_init(BufferSendX1[faceLeft].data(), bufferSizeX1, realMPI, procSend, 101,
                 mygrid->CartComm, &sendRequestX1[faceLeft]));
+  MPI_SAFE_CALL(MPI_Send_init(BufferSendX1New[faceLeft].data(), bufferSizeX1, realMPI, procSend,
+                1101,
+                mygrid->CartComm, &sendRequestX1New[faceLeft]));
 
   MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX1[faceRight].data(), bufferSizeX1, realMPI, procRecv, 101,
                 mygrid->CartComm, &recvRequestX1[faceRight]));
+  MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX1New[faceRight].data(), bufferSizeX1, realMPI, procRecv,
+                1101,
+                mygrid->CartComm, &recvRequestX1New[faceRight]));
 
   #if DIMENSIONS >= 2
   // We receive from procRecv, and we send to procSend
@@ -355,9 +395,15 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
 
   MPI_SAFE_CALL(MPI_Send_init(BufferSendX2[faceRight].data(), bufferSizeX2, realMPI, procSend, 200,
                 mygrid->CartComm, &sendRequestX2[faceRight]));
+  MPI_SAFE_CALL(MPI_Send_init(BufferSendX2New[faceRight].data(), bufferSizeX2, realMPI, procSend,
+                1200,
+                mygrid->CartComm, &sendRequestX2New[faceRight]));
 
   MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX2[faceLeft].data(), bufferSizeX2, realMPI, procRecv, 200,
                 mygrid->CartComm, &recvRequestX2[faceLeft]));
+  MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX2New[faceLeft].data(), bufferSizeX2, realMPI, procRecv,
+                1200,
+                mygrid->CartComm, &recvRequestX2New[faceLeft]));
 
   // Send to the left
   // We receive from procRecv, and we send to procSend
@@ -365,9 +411,15 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
 
   MPI_SAFE_CALL(MPI_Send_init(BufferSendX2[faceLeft].data(), bufferSizeX2, realMPI, procSend, 201,
                 mygrid->CartComm, &sendRequestX2[faceLeft]));
+  MPI_SAFE_CALL(MPI_Send_init(BufferSendX2New[faceLeft].data(), bufferSizeX2, realMPI, procSend,
+                1201,
+                mygrid->CartComm, &sendRequestX2New[faceLeft]));
 
   MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX2[faceRight].data(), bufferSizeX2, realMPI, procRecv, 201,
                 mygrid->CartComm, &recvRequestX2[faceRight]));
+  MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX2New[faceRight].data(), bufferSizeX2, realMPI, procRecv,
+                1201,
+                mygrid->CartComm, &recvRequestX2New[faceRight]));
   #endif
 
   #if DIMENSIONS == 3
@@ -376,9 +428,15 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
 
   MPI_SAFE_CALL(MPI_Send_init(BufferSendX3[faceRight].data(), bufferSizeX3, realMPI, procSend, 300,
                 mygrid->CartComm, &sendRequestX3[faceRight]));
+  MPI_SAFE_CALL(MPI_Send_init(BufferSendX3New[faceRight].data(), bufferSizeX3, realMPI, procSend,
+                1300,
+                mygrid->CartComm, &sendRequestX3New[faceRight]));
 
   MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX3[faceLeft].data(), bufferSizeX3, realMPI, procRecv, 300,
                 mygrid->CartComm, &recvRequestX3[faceLeft]));
+  MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX3New[faceLeft].data(), bufferSizeX3, realMPI, procRecv,
+                1300,
+                mygrid->CartComm, &recvRequestX3New[faceLeft]));
 
   // Send to the left
   // We receive from procRecv, and we send to procSend
@@ -386,9 +444,15 @@ ConstrainedTransport<Phys>::ConstrainedTransport(Input &input, Fluid<Phys> *hydr
 
   MPI_SAFE_CALL(MPI_Send_init(BufferSendX3[faceLeft].data(), bufferSizeX3, realMPI, procSend, 301,
                 mygrid->CartComm, &sendRequestX3[faceLeft]));
+  MPI_SAFE_CALL(MPI_Send_init(BufferSendX3New[faceLeft].data(), bufferSizeX3, realMPI, procSend,
+                1301,
+                mygrid->CartComm, &sendRequestX3New[faceLeft]));
 
   MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX3[faceRight].data(), bufferSizeX3, realMPI, procRecv, 301,
                 mygrid->CartComm, &recvRequestX3[faceRight]));
+  MPI_SAFE_CALL(MPI_Recv_init(BufferRecvX3New[faceRight].data(), bufferSizeX3, realMPI, procRecv,
+                1301,
+                mygrid->CartComm, &recvRequestX3New[faceRight]));
   #endif
 
 #endif  // WITH_MPI
@@ -410,15 +474,21 @@ ConstrainedTransport<Phys>::~ConstrainedTransport() {
     for(int i=0 ; i< 2; i++) {
       MPI_Request_free( &sendRequestX1[i]);
       MPI_Request_free( &recvRequestX1[i]);
+      MPI_Request_free( &sendRequestX1New[i]);
+      MPI_Request_free( &recvRequestX1New[i]);
 
     #if DIMENSIONS >= 2
       MPI_Request_free( &sendRequestX2[i]);
       MPI_Request_free( &recvRequestX2[i]);
+      MPI_Request_free( &sendRequestX2New[i]);
+      MPI_Request_free( &recvRequestX2New[i]);
     #endif
 
     #if DIMENSIONS == 3
       MPI_Request_free( &sendRequestX3[i]);
       MPI_Request_free( &recvRequestX3[i]);
+      MPI_Request_free( &sendRequestX3New[i]);
+      MPI_Request_free( &recvRequestX3New[i]);
     #endif
     }
     #endif

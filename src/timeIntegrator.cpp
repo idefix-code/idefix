e9827297e0e650524ced2dd73c40c57257a0381e
nscepi <63753204+nscepi@users.noreply.github.com>
2026-09-03 08:51:57 +0200

    Move CheckNan to end of loop on stages in timeIntegrator (#403)
    * Move CheckNan to end of loop on stages in timeIntegrator
    
        CheckNan was previously located after EvolveStage. However, EvolveStage
        updates the conservative variables and CheckNan checks the primitive
        variables. This reports the detection of a NaN to the next cycle. By
        moving CheckNan to the end of the loop on stages in timeIntegrator,
        so after the ConsToPrim, this PR ensures that we detect the NaN at
        the right cycle.
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Move CheckNan at end of integration cycle
    
    * Update pre-commit config
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- src/timeIntegrator.cpp
+++ src/timeIntegrator.cpp
@@ -306,14 +306,6 @@ void TimeIntegrator::Cycle(DataBlock &data) {
     // evolve dt accordingly
     data.t += data.dt;
 
-    // Look for Nans every now and then (this actually cost a lot of time on GPUs
-    // because streams are divergent)
-    if(ncycles%checkNanPeriodicity==0) {
-      if(data.CheckNan()>0) {
-        throw std::runtime_error(std::string("Nan found after integration cycle"));
-      }
-    }
-
     // Compute next time_step during first stage
     if(stage==0) {
       if(!haveFixedDt) {
@@ -353,6 +345,7 @@ void TimeIntegrator::Cycle(DataBlock &data) {
     // Add back fargo velocity so that boundary conditions are applied on the total V
     if(data.haveFargo) data.fargo->AddVelocity(data.t);
   }
+
   /////////////////////////////////////////////////
   // END STAGES LOOP                             //
   /////////////////////////////////////////////////
@@ -380,6 +373,14 @@ void TimeIntegrator::Cycle(DataBlock &data) {
 
   // Launch user step last
   data.LaunchUserStepLast();
+
+  // Look for Nans every now and then (this actually cost a lot of time on GPUs
+  // because streams are divergent)
+  if(ncycles%checkNanPeriodicity==0) {
+    if(data.CheckNan()>0) {
+      throw std::runtime_error(std::string("Nan found after integration cycle"));
+    }
+  }
 
   // Update current time (should have already been done, but this gets rid of roundoff errors)
   data.t=t0+data.dt;

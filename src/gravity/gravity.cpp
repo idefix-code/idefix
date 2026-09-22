5f660085e7edf46b2f5aa6763f1eeb87908a377c
Sebastien Valat <sebastien.valat@univ-grenoble-alpes.fr>
2026-09-22 21:07:23 +0200

    copyrights: fix a mail issue

--- src/gravity/gravity.cpp
+++ src/gravity/gravity.cpp
@@ -7,7 +7,7 @@
 //
 // Copyright(C) by :
 // - Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr> (IPAG/UGA/CNRS - 2021 - 2025)
-// - Antonin Borderies <89980449+Anto6453@users.noreply.github.com> (2024)
+// - Antonin Borderies <antonin.borderies@ens-lyon.fr> (ENS Lyon - 2024)
 // and other code contributors
 //
 // Licensed under CeCILL 2.1 License, see COPYING for more information
